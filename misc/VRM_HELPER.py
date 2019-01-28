014166829c8a0d7799e54755b02301d07820cae8
iCyP <tailefang+github@gmail.com>
2019-01-28 17:37:20 +0900

    Update VRM_HELPER.py

--- misc/VRM_HELPER.py
+++ misc/VRM_HELPER.py
@@ -65,30 +65,31 @@ class VRM_VALIDATOR(bpy.types.Operator):
 
 
     def execute(self,context):
+        messages = []
         print("validation start")
         armature_count = 0
         armature = None
         node_name_set = set()
         for obj in bpy.context.selected_objects:
             if obj.name in node_name_set:
-                print("VRM exporter need Nodes(mesh,bones) name is unique. {} is doubled.".format(obj.name))
+                messages.append("VRM exporter need Nodes(mesh,bones) name is unique. {} is doubled.".format(obj.name))
             node_name_set.add(obj.name)
             if obj.type != "EMPTY" and (obj.parent is not None and obj.parent.type != "ARMATURE" and obj.type == "MESH"):
                 if obj.location != Vector([0.0,0.0,0.0]):#mesh and armature origin is on [0,0,0]
-                    print("There are not on origine location object {}".format(obj.name))
+                    messages.append("There are not on origine location object {}".format(obj.name))
             if obj.type == "ARMATURE":
                 armature = obj
                 armature_count += 1
                 if armature_count > 2:#only one armature
-                    print("VRM exporter needs only one armature not some armatures")
+                    messages.append("VRM exporter needs only one armature not some armatures")
                 already_root_bone_exist = False
                 for bone in obj.data.bones:
                     if bone.name in node_name_set:#nodes name is unique
-                        print("VRM exporter need Nodes(mesh,bones) name is unique. {} is doubled".format(bone.name))
+                        messages.append("VRM exporter need Nodes(mesh,bones) name is unique. {} is doubled".format(bone.name))
                     node_name_set.add(bone.name)
                     if bone.parent == None: #root bone is only 1
                         if already_root_bone_exist:
-                            print("root bone is only one {},{} are root bone now".format(bone.name,already_root_bone_exist))
+                            messages.append("root bone is only one {},{} are root bone now".format(bone.name,already_root_bone_exist))
                         already_root_bone_exist = bone.name
                 #TODO: T_POSE,
                 require_human_bone_dic = {bone_tag : None for bone_tag in [
@@ -100,12 +101,12 @@ class VRM_VALIDATOR(bpy.types.Operator):
                     if "humanBone" in bone.keys():
                         if bone["humanBone"] in require_human_bone_dic.keys():
                             if require_human_bone_dic[bone["humanBone"]]:
-                                print("humanBone is doubled with {},{}".format(bone.name,require_human_bone_dic[bone["humanBone"]].name))
+                                messages.append("humanBone is doubled with {},{}".format(bone.name,require_human_bone_dic[bone["humanBone"]].name))
                             else:
                                 require_human_bone_dic[bone["humanBone"]] = bone
                 for k,v in require_human_bone_dic.items():
                     if v is None:
-                        print("humanBone: {} is not defined.".format(k))
+                        messages.append("humanBone: {} is not defined.".format(k))
                 defined_human_bone = ["jaw","leftShoulder","rightShoulder",
                 "leftEye","rightEye","upperChest","leftToes","rightToes",
                 "leftThumbProximal","leftThumbIntermediate","leftThumbDistal","leftIndexProximal",
@@ -121,10 +122,10 @@ class VRM_VALIDATOR(bpy.types.Operator):
 
             if obj.type == "MESH":
                 if len(obj.data.materials) == 0:
-                    print("There is no material in mesh {}".format(obj.name))
+                    messages.append("There is no material in mesh {}".format(obj.name))
                 for poly in obj.data.polygons:
                     if poly.loop_total > 3:#polygons need all triangle
-                        print("There are not Triangle faces in {}".format(obj.name))
+                        messages.append("There are not Triangle faces in {}".format(obj.name))
                 #TODO modifier applyed, vertex weight Bone exist, vertex weight numbers.
         used_image = []
         used_material_set = set()
@@ -132,6 +133,7 @@ class VRM_VALIDATOR(bpy.types.Operator):
             for mat in mesh.data.materials:
                 used_material_set.add(mat)
         for mat in used_material_set:
+            #fix texture fetching
             if mat.texture_slots is not None:
 	            used_image += [tex_slot.texture.image for tex_slot in mat.texture_slots if tex_slot is not None]
 		#thumbnail
@@ -141,11 +143,14 @@ class VRM_VALIDATOR(bpy.types.Operator):
             pass
         for img in used_image:
             if img.is_dirty or img.filepath =="":
-                print("{} is not saved, please save.".format(img.name))
-            if image.file_format.lower() not in ["png","jpeg"]:
-                print("GLTF texture format is PNG AND JPEG only")
+                messages.append("{} is not saved, please save.".format(img.name))
+            if img.file_format.lower() not in ["png","jpeg"]:
+                messages.append("GLTF texture format is PNG AND JPEG only")
 
 
         #TODO textblock_validate
+
+        for mes in messages:
+            print(mes)
         print("validation finished")
         return {"FINISHED"}
