499c1dc35923699cdfb623e7f7719615fe63579d
iCyP <tailefang+github@gmail.com>
2019-11-29 16:49:05 +0900

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -21,7 +21,7 @@ bl_info = {
     "name":"VRM_IMPORTER",
     "author": "iCyP",
     "version": (0, 76),
-    "blender": (2, 80, 0),
+    "blender": (2, 81, 0),
     "location": "File->Import",
     "description": "VRM Importer",
     "warning": "",
