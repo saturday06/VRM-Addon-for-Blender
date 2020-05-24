d3874540d39d752c6d323caa8fcf4c5d355fc2c4
Isamu Mogi <isamu@leafytree.jp>
2020-05-25 07:42:54 +0900

    Export test stub

--- test/stub.py
+++ test/stub.py
@@ -1,8 +1,20 @@
 import bpy
+import os
+import tempfile
 
 print("STUB! Blender=" + bpy.app.version_string)
 
 assert "vrm" in dir(bpy.ops.import_scene)
 assert "vrm" in dir(bpy.ops.export_scene)
+
+bpy.ops.object.add(type="ARMATURE", enter_editmode=True, location=(0, 0, 0))
+bpy.ops.object.editmode_toggle()
+bpy.ops.icyp.make_basic_armature()
+bpy.ops.vrm.model_validate()
+
+with tempfile.TemporaryDirectory() as temp_dir:
+    filepath = os.path.join(temp_dir, "out.vrm")
+    bpy.ops.export_scene.vrm(filepath=filepath)
+    assert os.path.getsize(filepath) > 0
 
 print("OK")
