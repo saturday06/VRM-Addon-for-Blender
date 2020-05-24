af71609deab0d2eef2222b482a47d62d72e0c4fe
Isamu Mogi <isamu@leafytree.jp>
2020-05-25 07:45:04 +0900

    Windows testing support

--- test/run.py
+++ test/run.py
@@ -3,31 +3,41 @@
 import os
 import subprocess
 import tempfile
+import platform
+import sys
 
+if platform.system() == "Windows":
+    import _winapi
+
+    exeext = ".exe"
+else:
+    exeext = ""
+
+blender_command = sys.argv[1] if len(sys.argv) > 1 else "blender" + exeext
 repository_root_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+user_scripts_dir = tempfile.mkdtemp()
+os.mkdir(os.path.join(user_scripts_dir, "addons"))
+addon_dir = os.path.join(user_scripts_dir, "addons", "io_scene_vrm_saturday06")
+if platform.system() == "Windows":
+    _winapi.CreateJunction(repository_root_dir, addon_dir)
+else:
+    os.symlink(repository_root_dir, addon_dir)
 
-with tempfile.TemporaryDirectory() as user_scripts_dir:
-    os.mkdir(os.path.join(user_scripts_dir, "addons"))
-    os.symlink(
-        repository_root_dir,
-        os.path.join(user_scripts_dir, "addons", "io_scene_vrm_saturday06"),
-    )
-
-    env = os.environ.copy()
-    env["BLENDER_USER_SCRIPTS"] = user_scripts_dir
-    subprocess.run(
-        [
-            "blender",
-            "-noaudio",  # sound system to None (less output on stdout)
-            "--background",  # run UI-less
-            "--factory-startup",  # factory settings
-            "--addons",
-            "io_scene_vrm_saturday06",  # enable the addon
-            "--python-exit-code",
-            "1",
-            "--python",
-            os.path.join(repository_root_dir, "test", "stub.py"),  # run the test script
-        ],
-        env=env,
-        check=True,
-    )
+env = os.environ.copy()
+env["BLENDER_USER_SCRIPTS"] = user_scripts_dir
+subprocess.run(
+    [
+        blender_command,
+        "-noaudio",  # sound system to None (less output on stdout)
+        "--factory-startup",  # factory settings
+        "--addons",
+        "io_scene_vrm_saturday06",  # enable the addon
+        "--python-exit-code",
+        "1",
+        "--background",
+        "--python",
+        os.path.join(repository_root_dir, "test", "stub.py"),  # run the test script
+    ],
+    env=env,
+    check=True,
+)
