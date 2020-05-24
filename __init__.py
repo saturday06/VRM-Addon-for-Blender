b9f6d46c468044a5904d3c19837cfa6e2b7d3bac
Isamu Mogi <isamu@leafytree.jp>
2020-05-24 21:49:32 +0900

    SemVer

--- __init__.py
+++ __init__.py
@@ -24,7 +24,7 @@ import os
 bl_info = {
     "name": "VRM_IMPORTER",
     "author": "saturday06, iCyP",
-    "version": (0, 80),
+    "version": (0, 80, 0),
     "blender": (2, 82, 0),
     "location": "File->Import",
     "description": "VRM Importer",
--- misc/version.py
+++ misc/version.py
@@ -1,1 +1,1 @@
-addon_version = (0, 80)
+addon_version = (0, 80, 0)
