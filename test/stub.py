26a774283c8d18c5687c6c943388b5c836170598
Isamu Mogi <isamu@leafytree.jp>
2020-06-06 01:40:53 +0900

    Import test stub

--- test/stub.py
+++ test/stub.py
@@ -11,8 +11,20 @@ bpy.ops.icyp.make_basic_armature()
 bpy.ops.vrm.model_validate()
 
 with tempfile.TemporaryDirectory() as temp_dir:
+    bpy.data.objects["skeleton"].select_set(True)
     filepath = os.path.join(temp_dir, "out.vrm")
     bpy.ops.export_scene.vrm(filepath=filepath)
     assert os.path.getsize(filepath) > 0
+
+    bpy.ops.wm.read_factory_settings()
+
+    bpy.ops.import_scene.vrm(filepath=filepath)
+    bpy.data.objects["skeleton"].select_set(True)
+    filepath2 = os.path.join(temp_dir, "out2.vrm")
+    bpy.ops.export_scene.vrm(filepath=filepath2)
+    assert os.path.getsize(filepath2) > 0
+
+    # TODO: deterministic
+    # assert pathlib.Path(filepath).read_bytes() == pathlib.Path(filepath2).read_bytes()
 
 print("OK")
