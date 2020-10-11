2049bfa5060006f32b2edc8f525ce9cd7ea2401f
Isamu Mogi <isamu@leafytree.jp>
2020-10-11 20:56:01 +0900

    Version bump

--- __init__.py
+++ __init__.py
@@ -26,7 +26,7 @@ bl_info = {
     "name": "VRM_IMPORTER",
     "author": "saturday06, iCyP",
     # I'd like to use version.version(). But that is not allowed.
-    "version": (0, 97, 0),
+    "version": (0, 98, 0),
     "blender": (2, 82, 0),
     "location": "File->Import",
     "description": "VRM Importer",
