245a2ba55dcdd7d5b463602b260090f86b3d0578
Isamu Mogi <isamu@leafytree.jp>
2020-10-17 08:43:10 +0900

    Extract GLSL

---
+++ misc/depth.frag.glsl
@@ -0,0 +1,3 @@
+void main() {
+    gl_FragColor = vec4(vec3(gl_FragCoord.z), 1);
+}
---
+++ misc/depth.vert.glsl
@@ -0,0 +1,8 @@
+in vec3 position;
+uniform mat4 depthMVP;
+uniform mat4 obj_matrix;
+
+void main()
+{
+    gl_Position = depthMVP * obj_matrix * vec4(position,1);
+}
