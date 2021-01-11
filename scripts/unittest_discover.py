6dc457e7af4fcdf0e937effd570dbe392589b46b
Isamu Mogi <isamu@leafytree.jp>
2021-01-11 10:24:41 +0900

    Fix exit code

--- scripts/unittest_discover.py
+++ scripts/unittest_discover.py
@@ -6,4 +6,5 @@ from os.path import dirname
 test = unittest.TestLoader().discover(start_dir=dirname(dirname(__file__)))
 runner = unittest.runner.TextTestRunner()
 result = runner.run(test)
-sys.exit(result.wasSuccessful())
+if not result.wasSuccessful():
+    raise Exception("Failed")
