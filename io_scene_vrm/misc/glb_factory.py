fcae6d6e583a8129ae3a6f05ab88841868bd8d1b
saturday06 <isamu@leafytree.jp>
2021-02-10 06:36:10 +0900

    glb packer func

--- io_scene_vrm/misc/glb_factory.py
+++ io_scene_vrm/misc/glb_factory.py
@@ -1817,25 +1817,28 @@ class GlbObj:
             del self.json_dic["meshes"]
         if not self.json_dic["materials"]:
             del self.json_dic["materials"]
-        magic = b"glTF" + struct.pack("<I", 2)
-        json_str = json.dumps(self.json_dic).encode("utf-8")
-        if len(json_str) % 4 != 0:
-            json_str += b"\x20" * (4 - len(json_str) % 4)
-        json_size = struct.pack("<I", len(json_str))
-        if len(self.bin) % 4 != 0:
-            for _ in range(4 - len(self.bin) % 4):
-                self.bin += b"\x00"
-        bin_size = struct.pack("<I", len(self.bin))
-        total_size = struct.pack(
-            "<I", len(json_str) + len(self.bin) + 28
-        )  # include header size
-        self.result = (
-            magic
-            + total_size
-            + json_size
-            + b"JSON"
-            + json_str
-            + bin_size
-            + b"BIN\x00"
-            + self.bin
-        )
+        self.result = pack_glb(self.json_dic, self.bin)
+
+
+def pack_glb(json_dict: Dict[str, Any], binary_chunk: bytes) -> bytes:
+    magic = b"glTF" + struct.pack("<I", 2)
+    json_str = json.dumps(json_dict).encode("utf-8")
+    if len(json_str) % 4 != 0:
+        json_str += b"\x20" * (4 - len(json_str) % 4)
+    json_size = struct.pack("<I", len(json_str))
+    if len(binary_chunk) % 4 != 0:
+        binary_chunk += b"\x00" * (4 - len(binary_chunk) % 4)
+    bin_size = struct.pack("<I", len(binary_chunk))
+    total_size = struct.pack(
+        "<I", len(json_str) + len(binary_chunk) + 28
+    )  # include header size
+    return (
+        magic
+        + total_size
+        + json_size
+        + b"JSON"
+        + json_str
+        + bin_size
+        + b"BIN\x00"
+        + binary_chunk
+    )
