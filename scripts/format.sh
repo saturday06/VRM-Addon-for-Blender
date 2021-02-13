d946182d4fb90871a3428d03081294051ae9341d
Isamu Mogi <isamu@leafytree.jp>
2021-02-14 03:43:10 +0900

    Auto cd

--- scripts/format.sh
+++ scripts/format.sh
@@ -2,6 +2,8 @@
 
 set -eux
 
+cd "$(dirname "$0")/.."
+
 git ls-files "*.py" | xargs -d '\n' poetry run autoflake --in-place --remove-all-unused-imports --remove-unused-variables
 git ls-files "*.py" | xargs -d '\n' poetry run isort
 git ls-files "*.py" | xargs -d '\n' poetry run black
--- scripts/lint.sh
+++ scripts/lint.sh
@@ -2,6 +2,8 @@
 
 set -eux
 
+cd "$(dirname "$0")/.."
+
 git ls-files "*.sh" | xargs -d '\n' shellcheck
 git ls-files "*.py" | xargs -d '\n' poetry run mypy --show-error-codes
 git ls-files "*.py" | xargs -d '\n' poetry run flake8 --count --show-source --statistics
