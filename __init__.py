c11bdb6f5eb1cc86345bfc68ef8cd032a1247dd4
saturday06 <isamu@leafytree.jp>
2021-03-05 20:52:22 +0900

    Version 1.4.1

--- __init__.py
+++ __init__.py
@@ -44,7 +44,7 @@ if "bl_info" in locals():
 bl_info = {
     "name": "VRM format",
     "author": "saturday06, iCyP",
-    "version": (1, 4, 0),
+    "version": (1, 4, 1),
     "blender": (2, 82, 0),
     "location": "File > Import-Export",
     "description": "Import-Edit-Export VRM",
