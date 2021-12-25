3ef0edf83991ddf1197ca11b51ca67ddcab69ab1
saturday06 <isamu@leafytree.jp>
2021-12-26 01:35:43 +0900

    Use function

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,13 @@
-import importlib
-from os.path import dirname, join
+def generate_dynamic_tests() -> None:
+    from importlib import machinery
+    from os.path import dirname, join
 
-# pylint: disable=no-value-for-parameter,deprecated-method
-importlib.machinery.SourceFileLoader(
-    "blender_vrm_addon_run_scripts_generate_dynamic_tests",
-    join(dirname(dirname(__file__)), "scripts", "generate_dynamic_tests.py"),
-).load_module()
-# pylint: enable=no-value-for-parameter,deprecated-method
+    # pylint: disable=no-value-for-parameter,deprecated-method
+    machinery.SourceFileLoader(
+        "blender_vrm_addon_run_scripts_generate_dynamic_tests",
+        join(dirname(dirname(__file__)), "scripts", "generate_dynamic_tests.py"),
+    ).load_module()
+    # pylint: enable=no-value-for-parameter,deprecated-method
+
+
+generate_dynamic_tests()
