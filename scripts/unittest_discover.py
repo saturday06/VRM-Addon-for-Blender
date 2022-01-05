fb96b4fdaa1ffb17d1937960cb8dc6c1c3e4ea26
saturday06 <isamu@leafytree.jp>
2022-01-05 23:08:29 +0900

    Add -f, --failfast option

--- scripts/unittest_discover.py
+++ scripts/unittest_discover.py
@@ -1,15 +1,34 @@
 #!/usr/bin/env python3
 
+import argparse
+import contextlib
 import platform
 import sys
 import unittest
-from os.path import dirname
+from os.path import basename, dirname
 from typing import TextIO
 
 
 def run(stream: TextIO) -> None:
+    argv = sys.argv
+    in_blender = False
+    with contextlib.suppress(ImportError):
+        import bpy
+
+        in_blender = bool(bpy.app.binary_path)
+
+    if in_blender:
+        if "--" in argv:
+            argv = argv[argv.index("--") + 1 :]
+        else:
+            argv = []
+
+    parser = argparse.ArgumentParser(prog=basename(__file__))
+    parser.add_argument("-f", "--failfast", action="store_true")
+    args = parser.parse_args(argv)
+
     test = unittest.TestLoader().discover(start_dir=dirname(dirname(__file__)))
-    runner = unittest.runner.TextTestRunner(stream=stream)
+    runner = unittest.runner.TextTestRunner(stream=stream, failfast=args.failfast)
     result = runner.run(test)
     if not result.wasSuccessful():
         sys.exit(1)
