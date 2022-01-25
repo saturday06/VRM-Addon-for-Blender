ad8cf5ac586d71767faca67f7510b1a8e1765e9a
Isamu Mogi <isamu@leafytree.jp>
2022-01-25 22:11:41 +0900

    Remove the reload_package()

--- __init__.py
+++ __init__.py
@@ -26,34 +26,6 @@ bl_info = {
 }
 
 
-# Script reloading (if the user calls 'Reload Scripts' from Blender)
-# https://github.com/KhronosGroup/glTF-Blender-IO/blob/04e26bef903543d08947c5a9a5fea4e787b68f17/addons/io_scene_gltf2/__init__.py#L32-L54
-# http://www.apache.org/licenses/LICENSE-2.0
-def reload_package(module_dict_main: dict) -> None:  # type: ignore[type-arg]
-    # Lazy import to minimize initialization before reload_package()
-    import importlib
-    from pathlib import Path
-    from typing import Any, Dict
-
-    def reload_package_recursive(
-        current_dir: Path, module_dict: Dict[str, Any]
-    ) -> None:
-        for path in current_dir.iterdir():
-            if "__init__" in str(path) or path.stem not in module_dict:
-                continue
-
-            if path.is_file() and path.suffix == ".py":
-                importlib.reload(module_dict[path.stem])
-            elif path.is_dir():
-                reload_package_recursive(path, module_dict[path.stem].__dict__)
-
-    reload_package_recursive(Path(__file__).parent, module_dict_main)
-
-
-if "bpy" in locals():
-    reload_package(locals())
-
-
 def register() -> None:
     import bpy
 
