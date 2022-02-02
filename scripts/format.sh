b4c3bf04d4d02b282e1eb22c65819e30362d70f1
Isamu Mogi <isamu@leafytree.jp>
2022-02-02 19:51:20 +0900

    macOS-compatible

--- scripts/format.sh
+++ scripts/format.sh
@@ -4,6 +4,6 @@ set -eux
 
 cd "$(dirname "$0")/.."
 
-git ls-files "*.py" | xargs -d '\n' poetry run autoflake --in-place --remove-all-unused-imports --remove-unused-variables
-git ls-files "*.py" | xargs -d '\n' poetry run isort
-git ls-files "*.py" | xargs -d '\n' poetry run black
+git ls-files "*.py" | xargs poetry run autoflake --in-place --remove-all-unused-imports --remove-unused-variables
+git ls-files "*.py" | xargs poetry run isort
+git ls-files "*.py" | xargs poetry run black
--- scripts/lint.sh
+++ scripts/lint.sh
@@ -4,6 +4,6 @@ set -eux
 
 cd "$(dirname "$0")/.."
 
-git ls-files "*.sh" | xargs -d '\n' shellcheck
-git ls-files "*.py" | xargs -d '\n' poetry run mypy --show-error-codes
-git ls-files "*.py" | xargs -d '\n' poetry run flake8 --count --show-source --statistics
+git ls-files "*.sh" | xargs shellcheck
+git ls-files "*.py" | xargs poetry run mypy --show-error-codes
+git ls-files "*.py" | xargs poetry run flake8 --count --show-source --statistics
