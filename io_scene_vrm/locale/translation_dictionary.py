9d172fbf055258012dce156125ae8b261aa5f59a
空想幻灵 <42439330+uitcis@users.noreply.github.com>
2022-02-25 10:27:31 +0800

    Update translation_dictionary.py

--- io_scene_vrm/locale/translation_dictionary.py
+++ io_scene_vrm/locale/translation_dictionary.py
@@ -3,3 +3,7 @@ from . import ja_jp
 translation_dictionary = {
     ja_jp.locale_key: ja_jp.translation_dictionary,
 }
+from . import zh_CN
+translation_dictionary = {
+    zh_CN.locale_key: zh_CN.translation_dictionary,
+}
