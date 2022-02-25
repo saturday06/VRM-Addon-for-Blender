579de7b877667bd9db45c748b1a1440c18efe352
Isamu Mogi <isamu@leafytree.jp>
2022-02-25 20:19:42 +0900

    Don't overwrite

--- io_scene_vrm/locale/translation_dictionary.py
+++ io_scene_vrm/locale/translation_dictionary.py
@@ -2,8 +2,5 @@ from . import ja_jp, zh_cn
 
 translation_dictionary = {
     ja_jp.locale_key: ja_jp.translation_dictionary,
-}
-
-translation_dictionary = {
-    zh_CN.locale_key: zh_CN.translation_dictionary,
+    zh_cn.locale_key: zh_cn.translation_dictionary,
 }
