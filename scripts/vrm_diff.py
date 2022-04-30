bde112a96f365bef16d47d2fe2f29945e9ee01a4
Isamu Mogi <isamu@leafytree.jp>
2022-04-30 19:56:34 +0900

    Guard fake-bpy-module

--- scripts/vrm_diff.py
+++ scripts/vrm_diff.py
@@ -3,9 +3,13 @@
 import sys
 from pathlib import Path
 
+from io_scene_vrm.external.fake_bpy_module_support import is_fake_bpy_module
 from io_scene_vrm.importer.vrm_diff import vrm_diff
 
 if __name__ == "__main__":
+    if is_fake_bpy_module():
+        print("bpy module is 'fake_bpy_module'. Real Blender bpy is required.")
+        sys.exit(1)
     float_tolerance = sys.float_info.epsilon
     if len(sys.argv) == 4:
         float_tolerance = float(sys.argv[3])
