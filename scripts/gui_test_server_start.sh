8549ff0d2e9318d088e6bba95f555aa7a4bf2324
Isamu Mogi <isamu@leafytree.jp>
2022-06-03 03:56:58 +0900

    Make shellcheck happy

--- scripts/gui_test_server_start.sh
+++ scripts/gui_test_server_start.sh
@@ -9,6 +9,7 @@ echo "$PWD" | sha256sum - | awk '{print $1}' > tmp/repository_root_path_hash.txt
 docker_hash=$(cat tmp/repository_root_path_hash.txt)
 tag_name="vrm_addon_for_blender_gui_test_$docker_hash"
 container_name="vrm_addon_for_blender_gui_test_container_$docker_hash"
+CI=$(set +u; echo "$CI")
 
 running_container=$(docker ps --quiet --filter "name=$container_name")
 if [ -n "$running_container" ]; then
@@ -19,7 +20,7 @@ docker build \
   . \
   --file scripts/gui_test_server.dockerfile \
   --tag "$tag_name" \
-  --build-arg "CI=$(set +u; echo "$CI")"
+  --build-arg "CI=$CI"
 
 publish=127.0.0.1:6080:6080/tcp
 if [ "$CI" = "true" ]; then
