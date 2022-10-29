d594bba669c6f4ec3d6b592f7a450409c19394e0
Isamu Mogi <isamu@leafytree.jp>
2022-10-29 20:16:59 +0900

    Fix wrong vertex group checking

--- io_scene_vrm/editor/validation.py
+++ io_scene_vrm/editor/validation.py
@@ -387,7 +387,7 @@ class WM_OT_vrm_validator(bpy.types.Operator):  # type: ignore[misc] # noqa: N80
                     weight_count = 0
                     for g in v.groups:
                         if (
-                            0 <= len(mesh_vertex_group_names) < g.group
+                            0 <= g.group < len(mesh_vertex_group_names)
                             and mesh_vertex_group_names[g.group] in bones_name
                             and g.weight < float_info.epsilon
                         ):
