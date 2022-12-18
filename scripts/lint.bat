b45ca97242d29bce1960661031585bc10aa35e76
Isamu Mogi <isamu@leafytree.jp>
2022-12-18 23:58:16 +0900

    Simple output

--- scripts/lint.bat
+++ scripts/lint.bat
@@ -1,14 +1,15 @@
+@echo off
 setlocal
 setlocal enabledelayedexpansion
 pushd "%~dp0.."
 set PYTHONUTF8=1
 for /f "tokens=* usebackq" %%f in (`git ls-files "*.py"`) do ( set py_files=!py_files! %%f )
+echo ### mypy ###
 call poetry run mypy --show-error-codes %py_files%
-echo on
+echo ### flake8 ###
 call poetry run flake8 --count --show-source --statistics %py_files%
-echo on
+echo ### pyright ###
 call poetry run pyright %py_files%
-echo on
 popd
 endlocal
 endlocal
