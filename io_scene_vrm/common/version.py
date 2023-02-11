01d1d81a50c54dcd5eab02b6e16f0254d5205fef
Isamu Mogi <isamu@leafytree.jp>
2023-02-12 08:41:22 +0900

    Use mutable global dataclass

--- io_scene_vrm/common/version.py
+++ io_scene_vrm/common/version.py
@@ -1,8 +1,9 @@
 import importlib
+from dataclasses import dataclass
 from os.path import getmtime
 from pathlib import Path
 from sys import float_info
-from typing import List, Optional, Tuple
+from typing import Optional, Tuple
 
 import bpy
 
@@ -12,12 +13,23 @@ logger = get_logger(__name__)
 
 MAX_SUPPORTED_BLENDER_MAJOR_MINOR_VERSION = (3, 4)
 
-last_blender_restart_required: List[bool] = []  # Mutable
-last_root_init_py_modification_time: List[float] = []  # Mutable
+
+@dataclass
+class Cache:
+    use: bool
+    last_blender_restart_required: bool
+    last_root_init_py_modification_time: float
 
 
-def clear_addon_version_cache() -> Optional[float]:
-    last_blender_restart_required.clear()
+cache = Cache(
+    use=False,
+    last_blender_restart_required=False,
+    last_root_init_py_modification_time=0.0,
+)
+
+
+def clear_addon_version_cache() -> Optional[float]:  # pylint: disable=useless-return
+    cache.use = False
     return None
 
 
@@ -42,46 +54,44 @@ def addon_version() -> Tuple[int, int, int]:
 
 
 def blender_restart_required() -> bool:
-    if last_blender_restart_required:
-        return last_blender_restart_required[0]
+    if cache.use:
+        return cache.last_blender_restart_required
 
-    last_blender_restart_required.clear()
-    last_blender_restart_required.append(False)
+    cache.use = True
+
+    if cache.last_blender_restart_required:
+        return True
 
     root_init_py_path = Path(__file__).parent.parent / "__init__.py"
     root_init_py_modification_time = getmtime(root_init_py_path)
     if (
-        last_root_init_py_modification_time
-        and abs(last_root_init_py_modification_time[0] - root_init_py_modification_time)
+        abs(cache.last_root_init_py_modification_time - root_init_py_modification_time)
         < float_info.epsilon
     ):
-        return last_blender_restart_required[0]
-    last_root_init_py_modification_time.clear()
-    last_root_init_py_modification_time.append(root_init_py_modification_time)
+        return False
+
+    cache.last_root_init_py_modification_time = root_init_py_modification_time
 
     spec = importlib.util.spec_from_file_location(
         "blender_vrm_addon_root_init_py_version_checking",
         root_init_py_path,
     )
     if spec is None:
-        logger.warning("Failed to create module spec")
-        return last_blender_restart_required[0]
+        return False
     mod = importlib.util.module_from_spec(spec)
     if spec.loader is None:
-        logger.warning("Failed to create module spec loader")
-        return last_blender_restart_required[0]
+        return False
     spec.loader.exec_module(mod)
 
     bl_info = getattr(mod, "bl_info", None)
     if not isinstance(bl_info, dict):
-        return last_blender_restart_required[0]
+        return False
     bl_info_version = bl_info.get("version")
     if bl_info_version == addon_version():
-        return last_blender_restart_required[0]
+        return False
 
-    last_blender_restart_required.clear()
-    last_blender_restart_required.append(True)
-    return last_blender_restart_required[0]
+    cache.last_blender_restart_required = True
+    return True
 
 
 def supported() -> bool:
