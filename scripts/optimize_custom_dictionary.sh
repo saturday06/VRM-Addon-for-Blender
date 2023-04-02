be97f0a86053ca41e83988c0af867db00a9221a6
Isamu Mogi <isamu@leafytree.jp>
2023-04-03 06:37:23 +0900

    Add exit condition

--- scripts/optimize_custom_dictionary.sh
+++ scripts/optimize_custom_dictionary.sh
@@ -6,6 +6,9 @@ cd "$(dirname "$0")/.."
 
 seq "$(wc -l dictionary.txt | awk '{print $1}')" | while read -r n; do
   while sed -e "${n}d" dictionary.txt >removed.tmp; do
+    if diff -u dictionary.txt removed.tmp; then
+      break
+    fi
     if ! (git ls-files "*.py" | xargs poetry run flake8 --enable-extensions flake8-spellcheck --whitelist removed.tmp); then
       break
     fi
