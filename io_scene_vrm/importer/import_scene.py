0078c2b9eb9bad6b7f681689a69908cf716f6429
Isamu Mogi <isamu@leafytree.jp>
2023-06-25 14:59:10 +0900

    Revert "data"
    This reverts commit e0b9305e98f22b10dfa99cacc609e55491ae4a90.

--- io_scene_vrm/importer/import_scene.py
+++ io_scene_vrm/importer/import_scene.py
@@ -5,7 +5,7 @@ import itertools
 import struct
 from os import environ
 from pathlib import Path
-from typing import Dict, List, Set, Tuple, Union, cast
+from typing import Dict, Set, Tuple, Union, cast
 from urllib.parse import urlparse
 
 import bpy
@@ -13,7 +13,6 @@ from bpy.app.translations import pgettext
 from bpy_extras.io_utils import ImportHelper
 
 from ..common import version
-from ..common.deep import Json
 from ..common.gl import GL_FLOAT
 from ..common.gltf import pack_glb, parse_glb
 from ..common.logging import get_logger
@@ -348,10 +347,10 @@ class IMPORT_SCENE_OT_vrma(bpy.types.Operator, ImportHelper):  # type: ignore[mi
 
 def generate_vrma(filepath: Path) -> None:
     human_bones_dict = {}
-    node_dicts: List[Dict[str, Json]] = []
-    accessor_dicts: List[Dict[str, Json]] = []
-    buffer_view_dicts: List[Dict[str, Json]] = []
-    buffer_dicts: List[Dict[str, Json]] = [{"byteLength": 0}]
+    node_dicts = []
+    accessor_dicts = []
+    buffer_view_dicts = []
+    buffer_dicts = [{"byteLength": 0}]
 
     hips_node_index = len(node_dicts)
     node_dicts.append(
@@ -371,7 +370,7 @@ def generate_vrma(filepath: Path) -> None:
     }
     inputs = list(input_output.keys())
     buffer_input_bytes = struct.pack("<" + "f" * len(inputs), *inputs)
-    buffer_input_dict: Dict[str, Json] = {
+    buffer_input_dict = {
         "uri": "data:application/gltf-buffer;base64,"
         + base64.b64encode(buffer_input_bytes).decode("ascii"),
         "byteLength": len(buffer_input_bytes),
@@ -379,7 +378,7 @@ def generate_vrma(filepath: Path) -> None:
     buffer_input_index = len(buffer_dicts)
     buffer_dicts.append(buffer_input_dict)
 
-    outputs: List[Tuple[float, float, float]] = list(functools.reduce(itertools.chain, input_output.values(), []))
+    outputs = list(functools.reduce(itertools.chain, input_output.values(), []))
     print(str(outputs))
     buffer_output_bytes = struct.pack("<" + "f" * len(outputs), *outputs)
     buffer_output_dict = {
