ff45c368e2030ddb4d685065ed81ba8e1920d83e
[BOT] Isamu Mogi <isamu@leafytree.jp>
2023-07-02 12:28:58 +0000

    docs: update the latest internal partial code [BOT]

--- __init__.py
+++ __init__.py
@@ -16,7 +16,7 @@ https://opensource.org/licenses/mit-license.php
 bl_info = {
     "name": "VRM format",
     "author": "saturday06, iCyP",
-    "version": (2, 17, 1),
+    "version": (2, 17, 2),
     "blender": (2, 93, 0),
     "location": "File > Import-Export",
     "description": "Import-Edit-Export VRM",
