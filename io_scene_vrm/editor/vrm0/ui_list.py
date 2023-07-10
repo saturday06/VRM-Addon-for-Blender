50c653f69061528ff738ab43629592f67810b1dd
Isamu Mogi <isamu@leafytree.jp>
2023-07-10 20:51:03 +0900

    chore: indentation

--- io_scene_vrm/editor/vrm0/ui_list.py
+++ io_scene_vrm/editor/vrm0/ui_list.py
@@ -28,20 +28,18 @@ class VRM_UL_vrm0_secondary_animation_group(bpy.types.UIList):  # type: ignore[m
         if not isinstance(bone_group, Vrm0SecondaryAnimationGroupPropertyGroup):
             return
 
-        if self.layout_type in {"DEFAULT", "COMPACT"}:
-            text = (
-                f"(Spring Bone {index})"
-                if bone_group.comment == ""
-                else bone_group.comment
-            )
-            if bone_group:
-                layout.label(text=text, icon="GROUP_BONE")
-                # layout.prop(bone_group, "comment", text="", emboss=False, icon_value=icon)
-            else:
-                layout.label(text="", translate=False, icon_value=icon)
-        elif self.layout_type in {"GRID"}:
+        if self.layout_type == "GRID":
             layout.alignment = "CENTER"
             layout.label(text="", icon_value=icon)
+            return
+
+        if self.layout_type not in {"DEFAULT", "COMPACT"}:
+            return
+
+        text = (
+            f"(Spring Bone {index})" if bone_group.comment == "" else bone_group.comment
+        )
+        layout.label(text=text, icon="GROUP_BONE")
 
 
 class VRM_UL_vrm0_secondary_animation_group_collider_group(bpy.types.UIList):  # type: ignore[misc]
@@ -65,14 +63,15 @@ class VRM_UL_vrm0_secondary_animation_group_collider_group(bpy.types.UIList):  #
         ):
             return
 
-        if self.layout_type in {"DEFAULT", "COMPACT"}:
-            if collider_group:
-                layout.label(text=collider_group.name, icon="SPHERE")
-            else:
-                layout.label(text="", translate=False, icon_value=icon)
-        elif self.layout_type in {"GRID"}:
+        if self.layout_type == "GRID":
             layout.alignment = "CENTER"
             layout.label(text="", icon_value=icon)
+            return
+
+        if self.layout_type not in {"DEFAULT", "COMPACT"}:
+            return
+
+        layout.label(text=collider_group.name, icon="SPHERE")
 
 
 class VRM_UL_vrm0_blend_shape_group(bpy.types.UIList):  # type: ignore[misc]
@@ -132,27 +131,26 @@ class VRM_UL_vrm0_blend_shape_bind(bpy.types.UIList):  # type: ignore[misc]
         if not isinstance(blend_shape_bind, Vrm0BlendShapeBindPropertyGroup):
             return
 
-        if self.layout_type in {"DEFAULT", "COMPACT"}:
-            if blend_shape_bind:
-                name = blend_shape_bind.mesh.mesh_object_name
-                mesh_object = blend_data.objects.get(
-                    blend_shape_bind.mesh.mesh_object_name
-                )
-                if (
-                    mesh_object
-                    and mesh_object.type == "MESH"
-                    and mesh_object.data
-                    and mesh_object.data.shape_keys
-                ):
-                    keys = mesh_object.data.shape_keys.key_blocks.keys()
-                    if blend_shape_bind.index in keys:
-                        name += " / " + blend_shape_bind.index
-                layout.label(text=name, icon="MESH_DATA")
-            else:
-                layout.label(text="", translate=False, icon_value=icon)
-        elif self.layout_type in {"GRID"}:
+        if self.layout_type == "GRID":
             layout.alignment = "CENTER"
             layout.label(text="", icon_value=icon)
+            return
+
+        if self.layout_type not in {"DEFAULT", "COMPACT"}:
+            return
+
+        name = blend_shape_bind.mesh.mesh_object_name
+        mesh_object = blend_data.objects.get(blend_shape_bind.mesh.mesh_object_name)
+        if (
+            mesh_object
+            and mesh_object.type == "MESH"
+            and mesh_object.data
+            and mesh_object.data.shape_keys
+        ):
+            keys = mesh_object.data.shape_keys.key_blocks.keys()
+            if blend_shape_bind.index in keys:
+                name += " / " + blend_shape_bind.index
+        layout.label(text=name, icon="MESH_DATA")
 
 
 class VRM_UL_vrm0_material_value_bind(bpy.types.UIList):  # type: ignore[misc]
@@ -174,16 +172,17 @@ class VRM_UL_vrm0_material_value_bind(bpy.types.UIList):  # type: ignore[misc]
         if not isinstance(material_value_bind, Vrm0MaterialValueBindPropertyGroup):
             return
 
-        if self.layout_type in {"DEFAULT", "COMPACT"}:
-            if material_value_bind:
-                name = ""
-                if material_value_bind.material:
-                    name = material_value_bind.material.name
-                    if material_value_bind.property_name:
-                        name += " / " + material_value_bind.property_name
-                layout.label(text=name, icon="MATERIAL")
-            else:
-                layout.label(text="", translate=False, icon_value=icon)
-        elif self.layout_type in {"GRID"}:
+        if self.layout_type == "GRID":
             layout.alignment = "CENTER"
             layout.label(text="", icon_value=icon)
+            return
+
+        if self.layout_type not in {"DEFAULT", "COMPACT"}:
+            return
+
+        name = ""
+        if material_value_bind.material:
+            name = material_value_bind.material.name
+            if material_value_bind.property_name:
+                name += " / " + material_value_bind.property_name
+        layout.label(text=name, icon="MATERIAL")
