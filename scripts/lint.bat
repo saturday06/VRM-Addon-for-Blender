b7066b5ad4b07fb92cdd3e2cca73a971e5244ace
Isamu Mogi <isamu@leafytree.jp>
2023-08-20 12:55:47 +0900

    chore: add pylint check

--- scripts/lint.bat
+++ scripts/lint.bat
@@ -8,6 +8,8 @@ echo ### mypy ###
 call poetry run mypy --show-error-codes %py_files%
 echo ### flake8 ###
 call poetry run flake8 --count --show-source --statistics %py_files%
+echo ### pylint ###
+call poetry run pylint %py_files%
 echo ### pyright ###
 call poetry run pyright %py_files%
 popd
--- scripts/lint.sh
+++ scripts/lint.sh
@@ -7,4 +7,5 @@ cd "$(dirname "$0")/.."
 git ls-files "*.sh" | xargs shellcheck
 git ls-files "*.py" | xargs poetry run mypy --show-error-codes
 git ls-files "*.py" | xargs poetry run flake8 --count --show-source --statistics
+git ls-files "*.py" | xargs poetry run pylint
 git ls-files "*.py" | xargs poetry run pyright
