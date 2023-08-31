f8bc4097880c34370b0c553b3531bb07432d5314
Isamu Mogi <isamu@leafytree.jp>
2023-08-31 20:54:53 +0900

    chore: disable autoflake because it conflicts with pyright

--- scripts/format.sh
+++ scripts/format.sh
@@ -4,7 +4,6 @@ set -eux
 
 cd "$(dirname "$0")/.."
 
-git ls-files "*.py" | xargs poetry run autoflake --in-place --remove-all-unused-imports --remove-unused-variables
 git ls-files "*.py" | xargs poetry run isort
 git ls-files "*.py" | xargs poetry run black
 git ls-files "*.sh" | xargs shfmt --write --simplify
