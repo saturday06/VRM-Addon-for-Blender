3b8af29c95b53b1731daadfd41964b7775450f7d
Isamu Mogi <isamu@leafytree.jp>
2023-11-17 00:32:36 +0900

    chore: bump version to 2.20.18

--- io_scene_vrm/__init__.py
+++ io_scene_vrm/__init__.py
@@ -17,7 +17,7 @@ https://opensource.org/licenses/mit-license.php
 bl_info = {
     "name": "VRM format",
     "author": "saturday06, iCyP",
-    "version": (2, 20, 17),
+    "version": (2, 20, 18),
     "blender": (2, 93, 0),
     "location": "File > Import-Export",
     "description": "Import-Edit-Export VRM",
