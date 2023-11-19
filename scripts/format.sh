dc41eb11aa4675d94503e8987089015c9bb902d9
Isamu Mogi <isamu@leafytree.jp>
2023-11-19 19:54:43 +0900

    chore: for old shfmt

--- scripts/format.sh
+++ scripts/format.sh
@@ -6,4 +6,4 @@ cd "$(dirname "$0")/.."
 
 git ls-files "*.py" | xargs poetry run isort
 git ls-files "*.py" | xargs poetry run black
-git ls-files "*.sh" | xargs shfmt --write --simplify
+git ls-files "*.sh" | xargs shfmt -w -s
