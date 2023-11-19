0f7b216b564b96ecaba9cd52634ac1bede2886a4
Isamu Mogi <isamu@leafytree.jp>
2023-11-20 00:09:27 +0900

    chore: change format order

--- scripts/format.bat
+++ scripts/format.bat
@@ -3,8 +3,8 @@ setlocal enabledelayedexpansion
 pushd "%~dp0.."
 set PYTHONUTF8=1
 for /f "tokens=* usebackq" %%f in (`git ls-files "*.py"`) do ( set py_files=!py_files! %%f )
-call poetry run ruff --fix %py_files%
 call poetry run ruff format %py_files%
+call poetry run ruff --fix %py_files%
 echo on
 popd
 endlocal
