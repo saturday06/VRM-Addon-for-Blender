f502e65a27640a745327042abf85d60bd6b992cb
Isamu Mogi <isamu@leafytree.jp>
2024-02-24 01:29:27 +0900

    chore: bump version to 2.20.32

--- src/io_scene_vrm/__init__.py
+++ src/io_scene_vrm/__init__.py
@@ -14,7 +14,7 @@
 bl_info = {
     "name": "VRM format",
     "author": "saturday06, iCyP",
-    "version": (2, 20, 31),
+    "version": (2, 20, 32),
     "blender": (2, 93, 0),
     "location": "File > Import-Export",
     "description": "Import-Edit-Export VRM",
