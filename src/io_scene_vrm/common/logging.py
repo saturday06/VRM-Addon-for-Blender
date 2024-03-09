7c3a800172c034bde519d4a70d553d4fa7cba598
Isamu Mogi <isamu@leafytree.jp>
2024-03-09 20:48:45 +0900

    chore: use LoggerAdapter instead

--- src/io_scene_vrm/common/logging.py
+++ src/io_scene_vrm/common/logging.py
@@ -1,35 +1,53 @@
 import logging as standard_logging
+import sys
+from collections.abc import Mapping
 from os import environ
+from types import TracebackType
+from typing import TYPE_CHECKING, Optional, Union
+
+# https://github.com/python/typeshed/issues/7855
+if TYPE_CHECKING or sys.version_info >= (3, 11):
+    LoggerAdapter = standard_logging.LoggerAdapter[standard_logging.Logger]
+else:
+    LoggerAdapter = standard_logging.LoggerAdapter
 
 
-class Logger:
-    def log_prefix(self, severity: str) -> str:
-        # https://github.com/saturday06/VRM-Addon-for-Blender/blob/2_20_32/src/io_scene_vrm/__init__.py#L132-L133
-        return f"[VRM Add-on:{severity}] "
-
-    def __init__(self, logger: standard_logging.Logger) -> None:
-        self.logger = logger
-        self.level = logger.level
-
-    def debug(self, message: str) -> None:
-        self.logger.debug(self.log_prefix("Debug") + message)
-
-    def info(self, message: str) -> None:
-        self.logger.info(self.log_prefix("Info") + message)
-
-    def warning(self, message: str) -> None:
-        self.logger.warning(self.log_prefix("Warning") + message)
-
-    def error(self, message: str) -> None:
-        self.logger.error(self.log_prefix("Error") + message)
-
-    def exception(self, message: str) -> None:
-        self.logger.exception(self.log_prefix("Exception") + message)
+class VrmAddonLoggerAdapter(LoggerAdapter):
+    def log(
+        self,
+        level: int,
+        msg: object,
+        *args: object,
+        exc_info: Union[
+            None,
+            bool,
+            Union[
+                tuple[type[BaseException], BaseException, Optional[TracebackType]],
+                tuple[None, None, None],
+            ],
+            BaseException,
+        ] = None,
+        stack_info: bool = False,
+        stacklevel: int = 1,
+        extra: Optional[Mapping[str, object]] = None,
+        **kwargs: object,
+    ) -> None:
+        level_name = standard_logging.getLevelName(level)
+        super().log(
+            level,
+            f"[VRM Add-on:{level_name}] {msg}",
+            *args,
+            exc_info=exc_info,
+            stack_info=stack_info,
+            stacklevel=stacklevel,
+            extra=extra,
+            **kwargs,
+        )
 
 
 # https://docs.python.org/3.7/library/logging.html#logging.getLogger
-def get_logger(name: str) -> Logger:
+def get_logger(name: str) -> VrmAddonLoggerAdapter:
     logger = standard_logging.getLogger(name)
     if environ.get("BLENDER_VRM_LOGGING_LEVEL_DEBUG") == "yes":
         logger.setLevel(standard_logging.DEBUG)
-    return Logger(logger)
+    return VrmAddonLoggerAdapter(logger, {})
