421b3b5ea8a43c0d3e2b458d1644c023366bcae6
Isamu Mogi <isamu@leafytree.jp>
2024-03-20 17:17:23 +0900

    chore: use official image

--- tools/devcontainer_post_start_command.sh
+++ tools/devcontainer_post_start_command.sh
@@ -3,10 +3,3 @@
 set -eux -o pipefail
 
 ln -fsv "$(poetry env info --path)" .venv-devcontainer
-
-git ls-files | while read -r f; do
-  case "$(git ls-files -s "$f" | awk '{print $1}')" in
-  100644) chmod 644 "$f" ;;
-  100755) chmod 755 "$f" ;;
-  esac
-done
