c915fb95c220b34205f87f56c526f2112f0062aa
Isamu Mogi <isamu@leafytree.jp>
2024-03-30 22:20:18 +0900

    chore: reduce log output

--- tools/devcontainer_post_start_command.sh
+++ tools/devcontainer_post_start_command.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-set -eux -o pipefail
+set -eu -o pipefail
 
 # .venvがdevcontainer外のものと混ざるのを防ぐため、
 # .devcontainer内に固有の.venvを作り
 # あとで標準のものと別名でリンクを貼る
 poetry config virtualenvs.in-project false
-poetry env info
-poetry run python --version # 稀にvenvが壊れていることがある。このコマンドで復元する。
-if ! ln -fsv "$(poetry env info --path)" .venv-devcontainer; then
+poetry run python -c "" # 稀にvenvが壊れていることがある。このコマンドで復元する。
+if ! ln -fs "$(poetry env info --path)" .venv-devcontainer; then
   sudo rm -f .venv-devcontainer
-  ln -fsv "$(poetry env info --path)" .venv-devcontainer
+  ln -fs "$(poetry env info --path)" .venv-devcontainer
 fi
 
 sudo ./tools/devcontainer_fixup_files.py
