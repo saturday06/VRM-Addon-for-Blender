cb59c6a3360fd3adabf67eb902669befed9715bd
saturday06 <isamu@leafytree.jp>
2024-04-02 19:16:41 +0000

    chore: download submodules on create

--- tools/devcontainer_post_create_command.sh
+++ tools/devcontainer_post_create_command.sh
@@ -2,6 +2,9 @@
 
 set -eu -o pipefail
 
+# いちおうサブモジュールを取得するが、ワーキングコピーの状態が悪いと失敗するので `|| true` を付与
+git submodule update --init --recursive || true
+
 # .venvがdevcontainer外のものと混ざるのを防ぐため、
 # .devcontainer内に固有の.venvを作り
 # あとで標準のものと別名でリンクを貼る
