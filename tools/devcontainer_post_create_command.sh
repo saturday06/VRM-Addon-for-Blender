938723bddad1db11c590c8b3efb869f69d8f994c
Isamu Mogi <isamu@leafytree.jp>
2024-04-03 23:19:09 +0900

    chore: suppress duplicated progress bar

--- tools/devcontainer_post_create_command.sh
+++ tools/devcontainer_post_create_command.sh
@@ -4,7 +4,9 @@ set -eu -o pipefail
 
 # 作業フォルダの所有者やパーミッションを設定する。
 # sudoが強力すぎるため、poetryは経由せずOSのパッケージのみを用いて実行する。
-sudo env PYTHONDONTWRITEBYTECODE=1 ./tools/devcontainer_fixup_files.py
+# ./devcontainer_post_start_command.shでも同様のコマンドを実行する。
+# そのためこのままだとプログレスバーが計二回出力されるが、それはイマイチなのでここでは非表示。
+sudo env PYTHONDONTWRITEBYTECODE=1 ./tools/devcontainer_fixup_files.py --no-progress-bar
 
 # いちおうサブモジュールを取得するが、作業フォルダの状態次第で失敗するので `|| true` を付与
 git submodule update --init --recursive || true
