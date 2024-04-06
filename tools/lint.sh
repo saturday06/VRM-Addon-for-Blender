e9c636a79fd617dd812d3772a7422fe91cca014e
Isamu Mogi <doyoubi@pixiv.co.jp>
2024-04-06 09:18:04 +0900

    feat: hadolint

--- tools/lint.sh
+++ tools/lint.sh
@@ -11,3 +11,4 @@ git ls-files "*.py" | xargs poetry run codespell
 git ls-files "*.py" | xargs poetry run mypy --show-error-codes
 git ls-files "*.py" | xargs poetry run pyright
 git ls-files "*.sh" | xargs shfmt -d -s
+git ls-files "*/Dockerfile" "*.dockerfile" | xargs hadolint
