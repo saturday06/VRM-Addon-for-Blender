33015e3054cdc6afa324420063515dbca20e6a05
Isamu Mogi <isamu@leafytree.jp>
2024-04-12 04:55:37 +0900

    chore: install extras on build container

--- tools/devcontainer_post_create_command.sh
+++ tools/devcontainer_post_create_command.sh
@@ -9,9 +9,6 @@ sudo env PYTHONDONTWRITEBYTECODE=1 ./tools/devcontainer_fixup_files.py
 # いちおうサブモジュールを取得するが、作業フォルダの状態次第で失敗するので `|| true` を付与
 git submodule update --init --recursive || true
 
-./tools/install_hugo.sh
-./tools/install_hadolint.sh
-
 # .venvがdevcontainer外のものと混ざるのを防ぐため、
 # .devcontainer内に固有の.venvを作り
 # あとで標準のものと別名でリンクを貼る
