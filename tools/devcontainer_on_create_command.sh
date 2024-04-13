defd247fcf86447dff6490c79df7863fa244e920
Isamu Mogi <isamu@leafytree.jp>
2024-04-14 08:52:05 +0900

    chore: tweak terminal prompt for poetry 1.8.2 + vscode 1.88.1 + python v2024.4.1

--- tools/devcontainer_on_create_command.sh
+++ tools/devcontainer_on_create_command.sh
@@ -8,6 +8,18 @@ sudo apt-get dist-upgrade --yes
 
 ./tools/devcontainer_fixup_workspace.sh
 
+# 空のvenvを作る。
+poetry run python -c ""
+
+# poetry 1.8.2が自動で作るvenvのスクリプトをVSCodeのターミナルが正しく解釈できないので手動で作る。
+# 例えばvenvの名前が "venv" で、シェルのデフォルトのプロンプトが "$" の場合、シェルのプロンプトは
+# 正しくは "(venv) $" となるべきだが、現状のpoetryとVSCodeの組み合わせでは "venv$" になっている。
+if [ "$(uname -m)" = "x86_64" ]; then
+  /usr/local/bin/python3 -m venv "$(poetry env info --path)" --clear --prompt venv
+else
+  /usr/local/bin/python3 -m venv "$(poetry env info --path)" --clear --prompt venv --system-site-packages
+fi
+
 poetry completions bash >>~/.bash_completion
 
 # いちおうサブモジュールを取得するが、作業フォルダの状態次第で失敗するので `|| true` を付与
