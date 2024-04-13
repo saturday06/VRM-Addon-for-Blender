ba1d7cd340e42cb18e7a2e44318e75958af34ed0
Isamu Mogi <isamu@leafytree.jp>
2024-04-14 02:03:15 +0900

    chore: add-on symbolic links

--- tools/devcontainer_on_create_command.sh
+++ tools/devcontainer_on_create_command.sh
@@ -17,3 +17,21 @@ for _ in $(seq 5); do
     break
   fi
 done
+
+# システムのBlenderから開発中のアドオンをすぐに動作確認できるようにする
+for blender_version in \
+  4.3 \
+  4.2 \
+  4.1 \
+  4.0 \
+  3.6 \
+  3.5 \
+  3.4 \
+  3.3 \
+  3.2 \
+  3.1 \
+  3.0 \
+  2.93; do
+  mkdir -p "$HOME/.config/blender/$blender_version/scripts/addons"
+  ln -fs "$PWD/src/io_scene_vrm" "$HOME/.config/blender/$blender_version/scripts/addons/"
+done
