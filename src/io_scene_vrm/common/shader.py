29879774a131f5fd57f78755bb0201058f731b8d
Isamu Mogi <isamu@leafytree.jp>
2024-05-26 18:17:20 +0900

    chore: fix link

--- src/io_scene_vrm/common/shader.py
+++ src/io_scene_vrm/common/shader.py
@@ -279,7 +279,7 @@ def load_mtoon1_shader(
 
     template_material = None
 
-    # https://git.blender.org/gitweb/gitweb.cgi/blender.git/blob/v2.83:/source/blender/windowmanager/intern/wm_files_link.c#l84
+    # https://projects.blender.org/blender/blender/src/tag/v2.93.18/source/blender/windowmanager/intern/wm_files_link.c#L85-L90
     if context.object is not None and context.object.mode == "EDIT":
         bpy.ops.object.mode_set(mode="OBJECT")
         edit_mode = True
