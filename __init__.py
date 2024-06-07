ae60ef4da652d1fe824d0811035f1dde6239f5ef
[BOT] Isamu Mogi <isamu@leafytree.jp>
2024-06-07 19:13:14 +0000

    docs: update the latest internal partial code to 0d80e184835110a1091b8db349ee5b89fd35d417 [BOT]

--- __init__.py
+++ __init__.py
@@ -15,7 +15,7 @@
 bl_info = {
     "name": "VRM format",
     "author": "saturday06, iCyP",
-    "version": (2, 20, 52),
+    "version": (2, 20, 53),
     "location": "File > Import-Export",
     "description": "Import-Edit-Export VRM",
     "blender": (2, 93, 0),
