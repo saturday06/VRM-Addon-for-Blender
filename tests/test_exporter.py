bee63bea7c693079bc15075046b5c85e20ec7868
Isamu Mogi <isamu@leafytree.jp>
2024-06-17 08:30:58 +0900

    chore: remove cast()

--- tests/test_exporter.py
+++ tests/test_exporter.py
@@ -1,6 +1,4 @@
 import sys
-from collections.abc import Sequence
-from typing import cast
 from unittest import TestCase
 
 
@@ -9,17 +7,18 @@ class TestExporter(TestCase):
         from io_scene_vrm.exporter import vrm0_exporter
 
         for arg, expected in [
-            ([1, 0, 0, 0], [1, 0, 0, 0]),
-            ([2, 0, 0, 0], [1, 0, 0, 0]),
-            ([1, 3, 0, 0], [0.25, 0.75, 0, 0]),
-            ([2, 2, 2, 2], [0.25, 0.25, 0.25, 0.25]),
-            ([0, 0, 0, sys.float_info.epsilon], [0, 0, 0, 1]),
-            ([0, sys.float_info.epsilon, 0, sys.float_info.epsilon], [0, 0.5, 0, 0.5]),
+            ([1.0, 0.0, 0.0, 0.0], [1.0, 0.0, 0.0, 0.0]),
+            ([2.0, 0.0, 0.0, 0.0], [1.0, 0.0, 0.0, 0.0]),
+            ([1.0, 3.0, 0.0, 0.0], [0.25, 0.75, 0.0, 0.0]),
+            ([2.0, 2.0, 2.0, 2.0], [0.25, 0.25, 0.25, 0.25]),
+            ([0.0, 0.0, 0.0, sys.float_info.epsilon], [0, 0, 0, 1]),
+            (
+                [0.0, sys.float_info.epsilon, 0, sys.float_info.epsilon],
+                [0, 0.5, 0, 0.5],
+            ),
         ]:
             with self.subTest(arg):
-                actual = vrm0_exporter.normalize_weights_compatible_with_gl_float(
-                    cast(Sequence[float], arg)
-                )
+                actual = vrm0_exporter.normalize_weights_compatible_with_gl_float(arg)
                 self.assertEqual(
                     expected, actual, f"Expected: {expected}, Actual: {actual}"
                 )
