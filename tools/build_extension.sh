ad41d52a08d74fd91f5955156b5e31085292f941
Isamu Mogi <isamu@leafytree.jp>
2024-06-22 21:40:37 +0900

    chore: use output-filepath instead

--- tools/build_extension.sh
+++ tools/build_extension.sh
@@ -21,15 +21,13 @@ git --no-pager diff
 
 rm -fr extension_output
 mkdir extension_output
+output_filepath="extension_output/vrm_$(LC_ALL=C date -u +%Y%m%d%H%M%S)_$(git rev-parse HEAD).zip"
 
 blender --command extension build \
   --source-dir src/io_scene_vrm \
-  --output-dir extension_output
+  --output-filepath "$output_filepath"
 
 blender --command extension validate \
-  extension_output/vrm.zip
-
-mv -v extension_output/vrm.zip \
-  "extension_output/vrm_$(LC_ALL=C date -u +%Y%m%d%H%M%S)_$(git rev-parse HEAD).zip"
+  "$output_filepath"
 
 git checkout .
