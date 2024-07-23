05a72645b677ab326b3fb071a4dd767d98bf1c50
空想幻灵 <42439330+uitcis@users.noreply.github.com>
2024-07-23 16:47:02 +0800

    Update translation_dictionary.py

--- src/io_scene_vrm/locale/translation_dictionary.py
+++ src/io_scene_vrm/locale/translation_dictionary.py
@@ -1,6 +1,6 @@
-from . import ja_jp, zh_cn
+from . import ja_jp, zh_HANS
 
 translation_dictionary = {
     ja_jp.locale_key: ja_jp.translation_dictionary,
-    zh_cn.locale_key: zh_cn.translation_dictionary,
+    zh_HANS.locale_key: zh_HANS.translation_dictionary,
 }
