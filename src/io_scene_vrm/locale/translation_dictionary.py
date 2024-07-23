44c4534194a267286680eefff034cd312e7a006e
Isamu Mogi <isamu@leafytree.jp>
2024-07-23 23:11:38 +0900

    chore: convert the file name to lower case to comply with Python standard convension

--- src/io_scene_vrm/locale/translation_dictionary.py
+++ src/io_scene_vrm/locale/translation_dictionary.py
@@ -1,6 +1,6 @@
-from . import ja_jp, zh_HANS
+from . import ja_jp, zh_hans
 
 translation_dictionary = {
     ja_jp.locale_key: ja_jp.translation_dictionary,
-    zh_HANS.locale_key: zh_HANS.translation_dictionary,
+    zh_hans.locale_key: zh_hans.translation_dictionary,
 }
