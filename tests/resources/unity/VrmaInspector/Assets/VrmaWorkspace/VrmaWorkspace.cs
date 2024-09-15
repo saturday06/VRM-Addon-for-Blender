63921330eaa3759777f43920270e6263d26e1512
Isamu Mogi <isamu@leafytree.jp>
2024-09-16 08:17:36 +0900

    chore: unity animation player setup

--- tests/resources/unity/VrmaInspector/Assets/VrmaWorkspace/VrmaWorkspace.cs
+++ tests/resources/unity/VrmaInspector/Assets/VrmaWorkspace/VrmaWorkspace.cs
@@ -7,17 +7,48 @@ namespace VrmaWorkspace
 {
     public class VrmaWorkspace : MonoBehaviour
     {
+        private Vrm10Instance? _vrm10Instance;
+
         private async void Start()
         {
-            var basePath = Path.Combine(Application.streamingAssetsPath, "Temp");
-            var vrmPath = Path.Combine(basePath, "input.vrm");
-            var vrmaPath = Path.Combine(basePath, "input.vrma");
+            var vrmaBasePath = Path.Combine(
+                Application.streamingAssetsPath,
+                "External",
+                "VRMA_MotionPack",
+                "vrma"
+            );
+            var vrmPath = Path.Combine(Application.streamingAssetsPath, "Example.vrm");
+            var vrmaPath = Path.Combine(vrmaBasePath, "VRMA_01.vrma");
             var _vrmBytes = await File.ReadAllBytesAsync(vrmPath);
+
+            Debug.LogFormat("VRM: {0}", vrmPath);
+            _vrm10Instance = await Vrm10.LoadPathAsync(
+                vrmPath,
+                canLoadVrm0X: true,
+                showMeshes: false
+            );
+            if (_vrm10Instance == null)
+            {
+                Debug.LogWarning("LoadPathAsync is null");
+                return;
+            }
+            // vrm10Instance.Runtime.VrmAnimation
+
+            var runtimeGltfInstance = _vrm10Instance.GetComponent<RuntimeGltfInstance>();
+            // runtimeGltfInstance.ShowMeshes();
+            runtimeGltfInstance.EnableUpdateWhenOffscreen();
+
             using GltfData data = new AutoGltfFileParser(vrmaPath).Parse();
             using var loader = new VrmAnimationImporter(data);
             var instance = await loader.LoadAsync(new ImmediateCaller());
-            var anim = instance.GetComponent<Vrm10AnimationInstance>();
-            // var vrmAnimation = new VrmAnimation(instance.GetComponent<VrmAnimationInstance>());
+            var a = instance.GetComponent<Vrm10AnimationInstance>();
+            // _vrm10Instance.Runtime.VrmAnimation = a;
+            instance.GetComponent<Animation>().Play();
+        }
+
+        private void Update()
+        {
+            // _vrm10Instance?.Runtime.Process();
         }
     }
 }
---
+++ tests/resources/unity/VrmaInspector/README.txt
@@ -0,0 +1,9 @@
+VRM Animation Inspector
+
+■ セットアップ手順
+
+1. BOOTHから「VRMアニメーション7種セット（.vrma）」をダウンロードします。
+   URLはこちら→ https://booth.pm/ja/items/5512385
+
+2. zipに含まれる「VRMA_MotionPack」フォルダを「Assets/StreamingAssets/External」フォルダに展開してます。
+   「Assets/StreamingAssets/External/VRMA_MotionPack/vrma/VRMA_01.vrma」が存在していればOKです。
