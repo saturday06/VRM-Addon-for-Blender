5aee3c07ad3f73f8457dfaa7629a2506829620cc
Isamu Mogi <isamu@leafytree.jp>
2024-09-16 14:17:50 +0900

    chore: fixup workspace permissions before tool installation

--- tools/devcontainer_update_content_command.sh
+++ tools/devcontainer_update_content_command.sh
@@ -5,9 +5,10 @@ set -eu -o pipefail
 # Dockerイメージは積極的にキャッシュされ、パッケージが古いままのことが多いのでここでアップデート
 sudo dnf update -y
 
-uv self update
+./tools/devcontainer_fixup_workspace.sh
 
 ./tools/install_hugo.sh
 ./tools/install_hadolint.sh
 ./tools/install_shfmt.sh
-./tools/devcontainer_fixup_workspace.sh
+
+uv self update
