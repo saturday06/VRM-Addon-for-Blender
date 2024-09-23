42603addb54a392721ee2320940daa68b13edd73
Isamu Mogi <isamu@leafytree.jp>
2024-09-23 10:00:02 +0900

    chore: pyright npm edition

--- tools/lint.bat
+++ tools/lint.bat
@@ -5,24 +5,27 @@ pushd "%~dp0.."
 set PYTHONUTF8=1
 for /f "tokens=* usebackq" %%f in (`git ls-files "*.py"`) do ( set py_files=!py_files! %%f )
 for /f "tokens=* usebackq" %%f in (`git ls-files "*.pyi"`) do ( set pyi_files=!pyi_files! %%f )
+
 echo ### ruff ###
 call uv run ruff check %py_files% %pyi_files%
 echo ### codespell ###
 call uv run codespell %py_files%
 echo ### mypy ###
 call uv run mypy --show-error-codes %py_files% %pyi_files%
-echo ### pyright ###
-call uv run pyright %py_files% %pyi_files%
 
 where npm
 if %errorlevel% equ 0 call npm install
 
+echo ### pyright ###
+where npm
+if %errorlevel% equ 0 call npm exec --yes -- pyright --warnings --pythonpath .venv\bin\python3.exe
 echo ### prettier ###
 where npm
 if %errorlevel% equ 0 call npm exec --yes -- prettier --write .
 echo ### vrm validator ###
 where npm
 if %errorlevel% equ 0 call npm exec --yes --package=gltf-validator -- node .\tools\vrm_validator.js
+
 popd
 endlocal
 endlocal
--- tools/lint.sh
+++ tools/lint.sh
@@ -8,9 +8,9 @@ git ls-files "*.sh" | xargs shellcheck
 git ls-files "*.py" "*.pyi" | xargs uv run ruff check
 git ls-files "*.py" "*.pyi" | xargs uv run codespell
 git ls-files "*.py" "*.pyi" | xargs uv run mypy --show-error-codes
-git ls-files "*.py" "*.pyi" | xargs uv run pyright
 git ls-files "*.sh" | xargs shfmt -d -s
 git ls-files "*/Dockerfile" "*.dockerfile" | xargs hadolint
 npm install
+npm exec --yes -- pyright --warnings --pythonpath .venv/bin/python3
 npm exec --yes -- prettier --check .
 npm exec --yes --package=gltf-validator -- node ./tools/vrm_validator.js
