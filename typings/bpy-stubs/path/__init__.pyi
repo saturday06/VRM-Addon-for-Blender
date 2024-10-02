37f63c1eac70a4c4973b5d3d90ba8bb8eec18fa1
Isamu Mogi <isamu@leafytree.jp>
2024-10-02 22:04:50 +0900

    chore: bpy.path

---
+++ typings/bpy-stubs/path/__init__.pyi
@@ -0,0 +1,11 @@
+from typing import Optional, Union
+
+from bpy.types import Library
+
+def abspath(
+    path: str,
+    start: Union[str, bytes, None] = None,
+    library: Optional[Library] = None,
+) -> str: ...
+def basename(path: str) -> str: ...
+def clean_name(name: str, replace: str = "_") -> str: ...
