95a22c974ee9c3a8e19d0f356c251caceefc66fd
Isamu Mogi <isamu@leafytree.jp>
2024-10-02 16:52:49 +0000

    chore: import bpy.path

--- typings/bpy-stubs/__init__.pyi
+++ typings/bpy-stubs/__init__.pyi
@@ -1,6 +1,7 @@
 from . import app as app
 from . import msgbus as msgbus
 from . import ops as ops
+from . import path as path
 from . import props as props
 from . import types as types
 from . import utils as utils
