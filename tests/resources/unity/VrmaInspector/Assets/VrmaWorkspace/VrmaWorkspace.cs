f56d36e422c98c56672bb763b1fd6040a1e5482e
Isamu Mogi <isamu@leafytree.jp>
2024-12-01 07:00:31 +0900

    chore: tweak debug viewer

--- tests/resources/unity/VrmaInspector/Assets/VrmaWorkspace/VrmaWorkspace.cs
+++ tests/resources/unity/VrmaInspector/Assets/VrmaWorkspace/VrmaWorkspace.cs
@@ -1,27 +1,43 @@
 // SPDX-License-Identifier: MIT OR GPL-3.0-or-later
+
+using System;
 using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using UniGLTF;
 using UnityEngine;
 using UniVRM10;
+using UnityEngine.InputSystem;
 
 namespace VrmaWorkspace
 {
     public class VrmaWorkspace : MonoBehaviour
     {
         private Vrm10Instance? _vrm10Instance;
+        private RuntimeGltfInstance? _vrmaInstance;
+        private string? _vrmaPath;
+        private byte[]? _vrmaBytes;
 
-        private async void Start()
+        private void Start()
         {
-            var vrmaBasePath = Path.Combine(
-                Application.streamingAssetsPath,
-                "External",
-                "VRMA_MotionPack",
-                "vrma"
-            );
-            var vrmPath = Path.Combine(Application.streamingAssetsPath, "Example.vrm");
-            var vrmaPath = Path.Combine(vrmaBasePath, "VRMA_01.vrma");
-            var _vrmBytes = await File.ReadAllBytesAsync(vrmPath);
+            _ = LoadVrma(interactive: true);
+        }
 
+        private void OnApplicationFocus(bool hasFocus)
+        {
+            if (!hasFocus)
+            {
+                return;
+            }
+
+            _ = LoadVrma(interactive: false);
+        }
+
+        private async Task LoadVrma(bool interactive)
+        {
+            UnityEngine.Debug.LogFormat("Loading Vrma interactive={0} ...", interactive);
+            /*
+            var vrmPath = Path.Combine(Application.streamingAssetsPath, "Example.vrm");
             Debug.LogFormat("VRM: {0}", vrmPath);
             _vrm10Instance = await Vrm10.LoadPathAsync(
                 vrmPath,
@@ -34,22 +50,90 @@ namespace VrmaWorkspace
                 return;
             }
             // vrm10Instance.Runtime.VrmAnimation
-
             var runtimeGltfInstance = _vrm10Instance.GetComponent<RuntimeGltfInstance>();
-            // runtimeGltfInstance.ShowMeshes();
-            runtimeGltfInstance.EnableUpdateWhenOffscreen();
+                // runtimeGltfInstance.ShowMeshes();
+                runtimeGltfInstance.EnableUpdateWhenOffscreen();
+            */
 
-            using GltfData data = new AutoGltfFileParser(vrmaPath).Parse();
+            _vrmaPath ??= ReadVrmaPath(interactive: interactive);
+            if (_vrmaPath == null)
+            {
+                if (interactive)
+                {
+                    UnityEngine.Debug.LogError($"No path");
+                }
+                return;
+            }
+
+            var vrmaBytes = await File.ReadAllBytesAsync(_vrmaPath);
+            if (_vrmaBytes != null)
+            {
+                if (vrmaBytes.SequenceEqual(_vrmaBytes))
+                {
+                    UnityEngine.Debug.LogFormat($"Not updated: {_vrmaPath}");
+                    return;
+                }
+            }
+            _vrmaBytes = vrmaBytes;
+
+            _vrmaInstance?.Dispose();
+
+            Debug.Log($"Loading Vrma from {_vrmaPath}");
+
+            using GltfData data = new AutoGltfFileParser(_vrmaPath).Parse();
             using var loader = new VrmAnimationImporter(data);
-            var instance = await loader.LoadAsync(new ImmediateCaller());
-            var a = instance.GetComponent<Vrm10AnimationInstance>();
+            _vrmaInstance = await loader.LoadAsync(new ImmediateCaller());
+            // var _a = _vrmaInstance.GetComponent<Vrm10AnimationInstance>();
             // _vrm10Instance.Runtime.VrmAnimation = a;
-            instance.GetComponent<Animation>().Play();
+            _vrmaInstance.GetComponent<Animation>().Play();
         }
 
+        /*
         private void Update()
         {
-            // _vrm10Instance?.Runtime.Process();
+            var mouse = Mouse.current;
+            if (mouse.leftButton.wasPressedThisFrame)
+            {
+                _vrmaPath = null;
+                _vrmaBytes = null;
+                _ = LoadVrma(interactive: true);
+            }
         }
+        */
+
+#if UNITY_EDITOR
+        private static string? ReadVrmaPath(bool interactive)
+        {
+            if (!interactive)
+            {
+                return null;
+            }
+            var path = UnityEditor.EditorUtility.OpenFilePanel(
+                "Open VRMA", "", "vrma"
+            );
+            if (string.IsNullOrEmpty(path) || !File.Exists(path))
+            {
+                return null;
+            }
+            return path;
+        }
+#else
+        private static string? ReadVrmaPath()
+        {
+            var vrmaBasePath = Path.Combine(
+                Application.streamingAssetsPath,
+                "External",
+                "VRMA_MotionPack",
+                "vrma"
+            );
+            var vrmaPath = Path.Combine(vrmaBasePath, "VRMA_01.vrma");
+            if (!File.Exists(vrmaPath))
+            {
+                Debug.LogWarning("Please setup vrma file. See README.txt for details.");
+                return null;
+            }
+            return vrmaPath;
+        }
+#endif
     }
 }
