3bb86f880ed7c8a8366df364431436d1d5e29194
Isamu Mogi <isamu@leafytree.jp>
2025-02-06 17:23:48 +0000

    chore: make mypy 1.15.0 happy

--- src/io_scene_vrm/editor/property_group.py
+++ src/io_scene_vrm/editor/property_group.py
@@ -485,7 +485,9 @@ class CollectionPropertyProtocol(Protocol[T_co]):
     def clear(self) -> None: ...  # TODO: undocumented
 
     @overload
-    def __getitem__(self, index: slice) -> tuple[T_co, ...]: ...  # TODO: undocumented
+    def __getitem__(
+        self, index: "slice[Optional[int], Optional[int], Optional[int]]"
+    ) -> tuple[T_co, ...]: ...  # TODO: undocumented
 
     @overload
     def __getitem__(self, index: int) -> T_co: ...  # TODO: undocumented
