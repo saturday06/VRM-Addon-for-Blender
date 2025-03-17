0acc8890880b393bd1afc4c7cbf02c475c83b5b7
Isamu Mogi <isamu@leafytree.jp>
2025-03-17 19:20:25 +0900

    feat: support Blender 4.4

--- src/io_scene_vrm/__init__.py
+++ src/io_scene_vrm/__init__.py
@@ -31,7 +31,7 @@ bl_info = {
     "category": "Import-Export",
 }
 
-MINIMUM_UNSUPPORTED_BLENDER_MAJOR_MINOR_VERSION = (4, 4)
+MINIMUM_UNSUPPORTED_BLENDER_MAJOR_MINOR_VERSION = (4, 5)
 
 
 def cleanse_modules() -> None:
