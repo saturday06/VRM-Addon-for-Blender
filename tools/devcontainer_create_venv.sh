ebffa7971d3f7db78c6bfc78b4aa965a10778b21
Isamu Mogi <isamu@leafytree.jp>
2025-05-18 05:52:11 +0900

    chore: ubuntu noble

--- tools/devcontainer_create_venv.sh
+++ tools/devcontainer_create_venv.sh
@@ -6,29 +6,15 @@ set -eu -o pipefail
 cd "$(dirname "$0")/.."
 
 create_venv() (
-  if [ "$(uname -m)" = "x86_64" ]; then
-    uv venv --prompt venv
-  else
-    # x86_64以外の場合はbpyパッケージが存在しないので、システムのものを使う
-    uv venv --prompt venv --python /usr/local/bin/python3 --system-site-packages
-  fi
+  uv venv --prompt venv
 )
 
 create=yes
 
-if [ "$(uname -m)" = "x86_64" ]; then
-  if grep -E "^home = /home/developer/.local/share/uv/python/" .venv/pyvenv.cfg >/dev/null &&
-    readlink -e .venv/bin/python >/dev/null \
-    ; then
-    create=no
-  fi
-else
-  if grep -E "^home = /usr/local/bin$" .venv/pyvenv.cfg >/dev/null &&
-    grep -E "^include-system-site-packages = true$" .venv/pyvenv.cfg >/dev/null &&
-    readlink -e .venv/bin/python >/dev/null \
-    ; then
-    create=no
-  fi
+if grep -E "^home = /home/developer/.local/share/uv/python/" .venv/pyvenv.cfg >/dev/null &&
+  readlink -e .venv/bin/python >/dev/null \
+  ; then
+  create=no
 fi
 
 if ([ "$create" = "yes" ] && ! create_venv) || ! uv run --isolated -- python -c ""; then
--- tools/devcontainer_update_content_command.sh
+++ tools/devcontainer_update_content_command.sh
@@ -6,17 +6,12 @@ set -eu -o pipefail
 cd "$(dirname "$0")/.."
 
 # Dockerイメージは積極的にキャッシュされ、パッケージが古いままのことが多いのでここでアップデート
-if command -v dnf; then
-  sudo dnf update -y
-elif command -v apt-get; then
-  sudo apt-get update
-  sudo DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -y
-fi
+sudo apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -y
 
 ./tools/devcontainer_fixup_workspace.sh
 
 ./tools/install_hadolint.sh
-./tools/install_shfmt.sh
 ./tools/install_editorconfig-checker.sh
 
 uv self update
