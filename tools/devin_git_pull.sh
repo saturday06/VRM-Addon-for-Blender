f89c0f008541861773f31deeccacfad11b1b3c66
Isamu Mogi <isamu@leafytree.jp>
2025-05-18 19:54:36 +0000

    chore devin's git pull configuration

---
+++ tools/devin_git_pull.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# SPDX-License-Identifier: MIT OR GPL-3.0-or-later
+
+# Please copy and pase this file to devin's 'Git Pull' code input area.
+cd ~/repos/VRM-Addon-for-Blender && git fetch --prune && git reset --hard origin/main && find . -mindepth 1 -maxdepth 1 -name .git -prune -o -exec rm -fr {} \; && git restore . && git submodule update --init --recursive
