6d1122fd055bb68f6478598d3f24f0e9acf9c1ae
Isamu Mogi <isamu@leafytree.jp>
2025-06-01 19:13:42 +0900

    chore: honor standard execution flow

--- tools/unittest_discover.py
+++ tools/unittest_discover.py
@@ -2,43 +2,41 @@
 # SPDX-License-Identifier: MIT OR GPL-3.0-or-later
 
 import argparse
-import contextlib
 import sys
 from pathlib import Path
 from typing import TextIO
 from unittest import TestLoader
 from unittest.runner import TextTestRunner
 
+import bpy
+
 # This is not necessary if executed from uv
 sys.path.append(str(Path(__file__).parent.parent / "src"))
 
 
-def run(stream: TextIO) -> None:
-    argv = sys.argv
-    in_blender = False
-    with contextlib.suppress(ImportError):
-        import bpy
-
-        in_blender = bool(bpy.app.binary_path)
-
-    if in_blender:
+def discover_and_run_test_suite(argv: list[str], stream: TextIO) -> int:
+    if bpy.app.binary_path:
         argv = argv[argv.index("--") + 1 :] if "--" in argv else []
 
     parser = argparse.ArgumentParser(prog=Path(__file__).name)
     parser.add_argument("-f", "--failfast", action="store_true")
     args = parser.parse_args(argv)
 
-    test = TestLoader().discover(start_dir=str(Path(__file__).parent.parent))
-    runner = TextTestRunner(stream=stream, failfast=args.failfast)
-    result = runner.run(test)
-    if not result.wasSuccessful():
-        sys.exit(1)
+    test_loader = TestLoader()
+    test_suite = test_loader.discover(start_dir=str(Path(__file__).parent.parent))
+    test_runner = TextTestRunner(stream=stream, failfast=args.failfast)
+    test_result = test_runner.run(test_suite)
+    return 0 if test_result.wasSuccessful() else 1
 
 
-if sys.platform == "win32":
-    with open(  # noqa: PTH123
-        sys.stderr.fileno(), mode="w", encoding="ansi", buffering=1
-    ) as windows_stderr:
-        run(windows_stderr)
-else:
-    run(sys.stderr)
+def main(argv: list[str]) -> int:
+    if sys.platform == "win32":
+        with open(  # noqa: PTH123
+            sys.stderr.fileno(), mode="w", encoding="ansi", buffering=1
+        ) as windows_stderr:
+            return discover_and_run_test_suite(argv, windows_stderr)
+    return discover_and_run_test_suite(argv, sys.stderr)
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
