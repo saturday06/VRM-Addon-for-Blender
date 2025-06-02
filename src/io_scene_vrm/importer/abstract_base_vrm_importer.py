b916ddede39dcc48628d4473847a026ff060c5c9
Isamu Mogi <isamu@leafytree.jp>
2025-06-03 08:48:59 +0900

    chore: write temporary object deletion errors to log

--- src/io_scene_vrm/importer/abstract_base_vrm_importer.py
+++ src/io_scene_vrm/importer/abstract_base_vrm_importer.py
@@ -1189,35 +1189,41 @@ class AbstractBaseVrmImporter(ABC):
             if self.is_temp_object_name(obj.name):
                 self.context.scene.collection.objects.unlink(obj)
 
-        while temp_object := next(
-            (
-                obj
-                for obj in self.context.blend_data.objects
-                if obj.users <= 0 and self.is_temp_object_name(obj.name)
-            ),
-            None,
-        ):
-            self.context.blend_data.objects.remove(temp_object)
+        for obj in list(self.context.blend_data.objects):
+            if not self.is_temp_object_name(obj.name):
+                continue
+            if obj.users > 0:
+                logger.warning(
+                    'Failed to remove "%s" with %d users while removing temp objects',
+                    obj.name,
+                    obj.users,
+                )
+            else:
+                self.context.blend_data.objects.remove(obj)
 
-        while temp_mesh := next(
-            (
-                mesh
-                for mesh in self.context.blend_data.meshes
-                if mesh.users <= 0 and self.is_temp_object_name(mesh.name)
-            ),
-            None,
-        ):
-            self.context.blend_data.meshes.remove(temp_mesh)
+        for mesh in list(self.context.blend_data.meshes):
+            if not self.is_temp_object_name(mesh.name):
+                continue
+            if mesh.users > 0:
+                logger.warning(
+                    'Failed to remove "%s" with %d users while removing temp meshes',
+                    mesh.name,
+                    mesh.users,
+                )
+            else:
+                self.context.blend_data.meshes.remove(mesh)
 
-        while temp_material := next(
-            (
-                material
-                for material in self.context.blend_data.materials
-                if material.users <= 0 and self.is_temp_object_name(material.name)
-            ),
-            None,
-        ):
-            self.context.blend_data.materials.remove(temp_material)
+        for material in list(self.context.blend_data.materials):
+            if not self.is_temp_object_name(material.name):
+                continue
+            if material.users > 0:
+                logger.warning(
+                    'Failed to remove "%s" with %d users while removing temp materials',
+                    material.name,
+                    material.users,
+                )
+            else:
+                self.context.blend_data.materials.remove(material)
 
         if self.armature is None:
             logger.warning("Failed to read VRM Humanoid")
