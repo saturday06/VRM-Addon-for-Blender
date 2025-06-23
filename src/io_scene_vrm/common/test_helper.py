3a7180fd12a54150f2193c7453f6893247b8cba5
Isamu Mogi <isamu@leafytree.jp>
2025-06-24 03:58:36 +0900

    chore: disable installed module in test

--- src/io_scene_vrm/common/test_helper.py
+++ src/io_scene_vrm/common/test_helper.py
@@ -1,16 +1,31 @@
 # SPDX-License-Identifier: MIT OR GPL-3.0-or-later
+from typing import Optional
 from unittest import TestCase
 
 import bpy
 
-MODULE = ".".join(__name__.split(".")[:-2])
+from .blender_manifest import BlenderManifest
+
+DEVELOPMENT_MODULE = ".".join(__name__.split(".")[:-2])
+MANIFEST_ID = BlenderManifest.read().id
 
 
 class AddonTestCase(TestCase):
+    disabled_installed_module: Optional[str]
+
     @classmethod
     def setUpClass(cls) -> None:
         super().setUpClass()
-        bpy.ops.preferences.addon_enable(module=MODULE)
+
+        cls.disabled_installed_module = None
+        for addon in bpy.context.preferences.addons:
+            module = addon.module
+            if not module.endswith("." + MANIFEST_ID):
+                continue
+            bpy.ops.preferences.addon_disable(module=module)
+            cls.disabled_installed_module = module
+
+        bpy.ops.preferences.addon_enable(module=DEVELOPMENT_MODULE)
 
     def setUp(self) -> None:
         super().setUp()
@@ -19,4 +34,11 @@ class AddonTestCase(TestCase):
     @classmethod
     def tearDownClass(cls) -> None:
         super().tearDownClass()
-        bpy.ops.preferences.addon_disable(module=MODULE)
+
+        bpy.ops.preferences.addon_disable(module=DEVELOPMENT_MODULE)
+
+        disabled_installed_module = cls.disabled_installed_module
+        if disabled_installed_module is not None:
+            bpy.ops.preferences.addon_enable(module=disabled_installed_module)
+
+        cls.disabled_installed_module = None
