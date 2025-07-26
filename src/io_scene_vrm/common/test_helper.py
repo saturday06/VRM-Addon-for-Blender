d7535ea50e41dc397d57b2687322f102cc21f49a
Isamu Mogi <isamu@leafytree.jp>
2025-07-27 01:42:38 +0900

    chore: reset preferences setUp/tearDown

--- src/io_scene_vrm/common/test_helper.py
+++ src/io_scene_vrm/common/test_helper.py
@@ -25,17 +25,18 @@ class AddonTestCase(TestCase):
             bpy.ops.preferences.addon_disable(module=module)
             cls.disabled_installed_module = module
 
-        bpy.ops.preferences.addon_enable(module=DEVELOPMENT_MODULE)
-
     def setUp(self) -> None:
         super().setUp()
+        bpy.ops.preferences.addon_enable(module=DEVELOPMENT_MODULE)
         bpy.ops.wm.read_homefile(use_empty=True)
+
+    def tearDown(self) -> None:
+        super().tearDown()
+        bpy.ops.preferences.addon_disable(module=DEVELOPMENT_MODULE)
 
     @classmethod
     def tearDownClass(cls) -> None:
         super().tearDownClass()
-
-        bpy.ops.preferences.addon_disable(module=DEVELOPMENT_MODULE)
 
         disabled_installed_module = cls.disabled_installed_module
         if disabled_installed_module is not None:
