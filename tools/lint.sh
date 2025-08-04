73b455025fb580c521299658bc6acffe2860b2bd
Isamu Mogi <isamu@leafytree.jp>
2025-08-05 08:35:50 +0900

    chore: move file name validation to lint.sh

--- tools/lint.sh
+++ tools/lint.sh
@@ -3,8 +3,28 @@
 
 set -eux -o pipefail
 
+validate_file_name_characters() (
+  set +x
+
+  git ls-files | while read -r f; do
+    encoding=$(echo "$f" | uchardet)
+    if [ "$encoding" != "ASCII" ]; then
+      echo "$f is not ascii file name but $encoding."
+      exit 1
+    fi
+  done
+
+  git ls-files "*.py" "*.pyi" | while read -r f; do
+    if [ "$f" != "$(echo "$f" | LC_ALL=C tr "[:upper:]" "[:lower:]")" ]; then
+      echo "$f contains uppercase character"
+      exit 1
+    fi
+  done
+)
+
 cd "$(dirname "$0")/.."
 
+validate_file_name_characters
 git ls-files "*.sh" | xargs shellcheck
 git ls-files "*.py" "*.pyi" | xargs uv run ruff check
 git ls-files "*.py" "*.pyi" | xargs uv run codespell
