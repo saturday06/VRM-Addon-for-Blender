7336f6143e32a5f1330fb1d0923e7ff5208aa8e1
Isamu Mogi <isamu@leafytree.jp>
2025-09-01 21:17:26 +0900

    chore: dedent

--- src/io_scene_vrm/common/workspace.py
+++ src/io_scene_vrm/common/workspace.py
@@ -204,17 +204,16 @@ def wm_append_without_library(
                 library.use_extra_user = False
 
             if library.users:
-                if bpy.app.version >= (3, 2):
-                    logger.warning(
-                        'Failed to remove "%s" with %d users'
-                        " while appending blend file:"
-                        ' filepath="%s" filename="%s" directory="%s"',
-                        library.name,
-                        library.users,
-                        append_filepath,
-                        append_filename,
-                        append_directory,
-                    )
+                logger.warning(
+                    'Failed to remove "%s" with %d users'
+                    " while appending blend file:"
+                    ' filepath="%s" filename="%s" directory="%s"',
+                    library.name,
+                    library.users,
+                    append_filepath,
+                    append_filename,
+                    append_directory,
+                )
             else:
                 context.blend_data.libraries.remove(library)
 
