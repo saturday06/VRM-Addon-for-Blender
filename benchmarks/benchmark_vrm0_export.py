980a234a761b43e224765f57a70e0069b3b9a58b
Isamu Mogi <isamu@leafytree.jp>
2025-10-05 11:13:38 +0000

    chore: use pure vrm0 model

--- benchmarks/benchmark_vrm0_export.py
+++ benchmarks/benchmark_vrm0_export.py
@@ -9,10 +9,7 @@ import requests
 from bpy.types import Armature, Context
 
 from io_scene_vrm.common import ops, version
-from io_scene_vrm.editor.extension import (
-    VrmAddonArmatureExtensionPropertyGroup,
-    get_armature_extension,
-)
+from io_scene_vrm.editor.extension import VrmAddonArmatureExtensionPropertyGroup
 
 addon_version = version.get_addon_version()
 spec_version = VrmAddonArmatureExtensionPropertyGroup.SPEC_VERSION_VRM1
@@ -25,8 +22,8 @@ def benchmark_vrm0_export(context: Context) -> None:
     blend_path = Path(__file__).parent / "temp" / "vrm0_export.blend"
     if not blend_path.exists():
         bpy.ops.wm.read_homefile(use_empty=True)
-        url = "https://raw.githubusercontent.com/vrm-c/vrm-specification/c24d76d99a18738dd2c266be1c83f089064a7b5e/samples/Seed-san/vrm/Seed-san.vrm"
-        path = Path(__file__).parent / "temp" / "Seed-san.vrm"
+        url = "https://raw.githubusercontent.com/pixiv/three-vrm/v0.6.11/packages/three-vrm/examples/models/three-vrm-girl.vrm"
+        path = Path(__file__).parent / "temp" / "three-vrm-girl.vrm"
         if not path.exists():
             with requests.get(url, timeout=5 * 60) as response:
                 assert response.ok
@@ -35,10 +32,8 @@ def benchmark_vrm0_export(context: Context) -> None:
         active_object = context.active_object
         if not active_object:
             raise AssertionError
-        if not isinstance(armature := active_object.data, Armature):
+        if not isinstance(active_object.data, Armature):
             raise TypeError
-        ext = get_armature_extension(armature)
-        ext.spec_version = ext.SPEC_VERSION_VRM0
         bpy.ops.wm.save_as_mainfile(filepath=str(blend_path))
         bpy.ops.wm.read_homefile(use_empty=True)
     bpy.ops.wm.open_mainfile(filepath=str(blend_path))
