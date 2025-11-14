3b4f6e8406b4989e65fee4ae792a35b42dc63b7d
Isamu Mogi <isamu@leafytree.jp>
2025-11-14 14:03:06 +0000

    chore: support for benchmark flamegraphs

--- tools/install_ci_codspeed_dependencies.sh
+++ tools/install_ci_codspeed_dependencies.sh
@@ -10,37 +10,41 @@ repository_root_path=$(
 )
 python_site_packages_path="${CMAKE_PREFIX_PATH}/lib/python3.12/site-packages"
 
+sudo mkdir -p "$CMAKE_PREFIX_PATH"
+sudo chown "$(id -u):$(id -g)" "$CMAKE_PREFIX_PATH"
+
 mkdir -p "$python_site_packages_path"
 
-mkdir -p /root/oneTBB/src
-cd /root/oneTBB/src
+mkdir -p "${HOME}/oneTBB/src"
+cd "${HOME}/oneTBB/src"
 # https://projects.blender.org/blender/lib-linux_x64/src/tag/v4.5.4/tbb/include/oneapi/tbb/version.h#L29-L36
 curl --fail --show-error --location --retry 5 --retry-all-errors --output ../oneTBB.tar.gz \
   "https://github.com/uxlfoundation/oneTBB/archive/refs/tags/v2021.13.0.tar.gz"
 tar zxf ../oneTBB.tar.gz --strip-components=1
 cmake -B build -S . -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$CMAKE_PREFIX_PATH" -DTBB_TEST=OFF
 cmake --build build --target install
-ldconfig
+sudo ldconfig
 
-mkdir -p /root/OpenImageIO/src
-cd /root/OpenImageIO/src
+mkdir -p "${HOME}/OpenImageIO/src"
+cd "${HOME}/OpenImageIO/src"
 # https://projects.blender.org/blender/lib-linux_x64/src/tag/v4.5.4/openimageio/include/OpenImageIO/oiioversion.h#L45-L49
 curl --fail --show-error --location --retry 5 --retry-all-errors --output ../OpenImageIO.tar.gz \
   "https://github.com/AcademySoftwareFoundation/OpenImageIO/releases/download/v3.0.6.1/OpenImageIO-3.0.6.1.tar.gz"
 tar zxf ../OpenImageIO.tar.gz --strip-components=1
 cmake -B build -S . -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$CMAKE_PREFIX_PATH" -DOpenImageIO_BUILD_MISSING_DEPS=all -DOpenImageIO_BUILD_TESTS=OFF -DOpenImageIO_BUILD_TOOLS=OFF
 cmake --build build --target install
-ldconfig
+sudo ldconfig
 
-mkdir -p /root/blender/blender
-cd /root/blender/blender
+mkdir -p "${HOME}/blender/blender"
+cd "${HOME}/blender/blender"
 curl --fail --show-error --location --output ../../blender.tar.xz \
   "https://download.blender.org/source/blender-4.5.4.tar.xz"
 test "$(md5sum ../../blender.tar.xz)" = "885c50f870e606a2ede06c43be7e4a6a  ../../blender.tar.xz"
 tar Jxf ../../blender.tar.xz --strip-components=1
 ./build_files/build_environment/install_linux_packages.py
-make NPROCS=1 "BUILD_CMAKE_ARGS=-DPYTHON_VERSION=3.12 -DWITH_VULKAN_BACKEND=OFF" bpy
-ldconfig
+numpy_include_path=$(python3 -c "import numpy; print(numpy.get_include())")
+make NPROCS=1 "BUILD_CMAKE_ARGS=-DPYTHON_VERSION=3.12 -DWITH_VULKAN_BACKEND=OFF '-DCMAKE_INCLUDE_PATH=${numpy_include_path}'" bpy
+sudo ldconfig
 
 cp -r ../build_linux_bpy/bin/* "$python_site_packages_path"
 
