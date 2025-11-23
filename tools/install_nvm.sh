625ea4991b34558fa9730b8c75cce376e5061d26
Isamu Mogi <isamu@leafytree.jp>
2025-11-23 12:55:18 +0000

    chore: wget

--- tools/install_nvm.sh
+++ tools/install_nvm.sh
@@ -3,13 +3,23 @@
 
 set -eu -o pipefail
 
-curl \
-  --fail \
-  --show-error \
-  --location \
-  --retry 20 \
-  --retry-all-errors \
-  --output install_nvm.tmp.sh \
-  "https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh"
+nvm_installer_url="https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh"
+if command -v curl; then
+  curl \
+    --fail \
+    --show-error \
+    --location \
+    --retry 20 \
+    --retry-all-errors \
+    --output install_nvm.tmp.sh \
+    "$nvm_installer_url"
+else
+  wget \
+    --tries=20 \
+    --retry-connrefused \
+    --retry-on-host-error \
+    --output-document=install_nvm.tmp.sh \
+    "$nvm_installer_url"
+fi
 bash install_nvm.tmp.sh
 rm install_nvm.tmp.sh
