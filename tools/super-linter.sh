f117ffca2275520e7fd678156b18f9e96381c7c1
Isamu Mogi <isamu@leafytree.jp>
2025-11-23 07:27:21 +0000

    chore: simplify

--- tools/super-linter.sh
+++ tools/super-linter.sh
@@ -11,17 +11,7 @@ else
   # To avoid duplication with docker images working in other folders or systems
   # Build using the combined hash value as the docker tag name
   pwd_and_system="$(pwd):$(uname -a)"
-  case "$(uname -s)" in
-  "Linux")
-    pwd_and_system_hash=$(echo "$pwd_and_system" | md5sum | cut -d" " -f 1)
-    ;;
-  "Darwin")
-    pwd_and_system_hash=$(echo "$pwd_and_system" | md5)
-    ;;
-  *)
-    exit 0
-    ;;
-  esac
+  pwd_and_system_hash=$(echo "$pwd_and_system" | md5sum | cut -d" " -f 1)
   super_linter_tag_name="super-linter-local-${pwd_and_system_hash}"
 fi
 
