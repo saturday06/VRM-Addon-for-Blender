953fe11a6b618f49e73bf29f0453804bf50751ad
Isamu Mogi <isamu@leafytree.jp>
2025-12-29 20:02:07 +0900

    chore: tweak cache

--- tools/super-linter.sh
+++ tools/super-linter.sh
@@ -5,8 +5,17 @@ set -eux
 
 cd "$(dirname "$0")/.."
 
+cached_image_path=
+cached_image_id=
 if [ "${CI:-}" = "true" ]; then
   super_linter_tag_name="super-linter-local"
+  if [ "${GITHUB_ACTIONS:-}" != "true" ]; then
+    cached_image_path=ci-super-linter-local-image.tar.gz
+    if [ -s "$cached_image_path" ]; then
+      docker image load --input "$cached_image_path"
+      cached_image_id=$(docker image inspect --format='{{.Id}}' "$super_linter_tag_name" || true)
+    fi
+  fi
 else
   # To avoid duplication with docker images working in other folders or systems
   # Build using the combined hash value as the docker tag name
@@ -15,15 +24,6 @@ else
   super_linter_tag_name="super-linter-local-${pwd_and_system_hash}"
 fi
 
-# Build the super-linter image for execution. Since the dependencies requiring
-# download are massive and download failures frequently occur under poor network
-# conditions, store the final image in the CI cache for reuse.
-ci_super_linter_local_image_path=ci-super-linter-local-image.tar.gz
-image_id=
-if [ "${CI:-}" = "true" ] && [ -s "$ci_super_linter_local_image_path" ]; then
-  docker image load --input "$ci_super_linter_local_image_path"
-  image_id=$(docker image inspect --format='{{.Id}}' "$super_linter_tag_name" || true)
-fi
 docker \
   build \
   --platform=linux/amd64 \
@@ -33,9 +33,13 @@ docker \
   --file \
   tools/super-linter.dockerfile \
   .
+
 new_image_id=$(docker image inspect --format='{{.Id}}' "$super_linter_tag_name")
-if [ "${CI:-}" = "true" ] && [ "$image_id" != "$new_image_id" ]; then
-  docker image save "$super_linter_tag_name" | gzip >"$ci_super_linter_local_image_path"
+if [ -n "$cached_image_path" ] && [ "$cached_image_id" != "$new_image_id" ]; then
+  # Build the super-linter image for execution. Since the dependencies requiring
+  # download are massive and download failures frequently occur under poor network
+  # conditions, store the final image in the CI cache for reuse.
+  docker image save "$super_linter_tag_name" | gzip >"$cached_image_path"
 fi
 
 # Run the built super-linter.
