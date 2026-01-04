82efb4da61658adaf6291962504bd883affbbfa3
Isamu Mogi <isamu@leafytree.jp>
2026-01-04 13:21:59 +0900

    Revert "chore: internalize the translation strings"
    This reverts commit 3e6cbeba3c77d0db27fbd7be38d7d29f91618ac1.

--- src/io_scene_vrm/locale/translation_dictionary.py
+++ src/io_scene_vrm/locale/translation_dictionary.py
@@ -1,21 +1,7 @@
 # SPDX-License-Identifier: MIT OR GPL-3.0-or-later
-import sys
-from typing import Final
+from . import ja_jp, zh_hans
 
-from . import ja_jp as __ja_jp
-from . import zh_hans as __zh_hans
-
-translation_dictionary: Final = {
-    locale_key: {
-        # Use sys.intern for the strings returned here to prevent them from being
-        # garbage collected. This is because EnumProperty item strings must be retained
-        # until Blender exits.
-        # https://docs.blender.org/api/2.93/bpy.props.html#bpy.props.EnumProperty
-        translation_key: sys.intern(translated_text)
-        for translation_key, translated_text in translation_dictionary.items()
-    }
-    for locale_key, translation_dictionary in {
-        __ja_jp.locale_key: __ja_jp.translation_dictionary,
-        __zh_hans.locale_key: __zh_hans.translation_dictionary,
-    }.items()
+translation_dictionary = {
+    ja_jp.locale_key: ja_jp.translation_dictionary,
+    zh_hans.locale_key: zh_hans.translation_dictionary,
 }
