a76841efc5303cf8d0e142df732d9c025957dffb
Isamu Mogi <isamu@leafytree.jp>
2026-02-11 22:32:20 +0900

    chore: improve make_test_method_name function for clarity and efficiency

--- src/io_scene_vrm/common/test_helper.py
+++ src/io_scene_vrm/common/test_helper.py
@@ -1,25 +1,38 @@
 # SPDX-License-Identifier: MIT OR GPL-3.0-or-later
-from typing import Optional
+from typing import Final, Optional
 from unittest import TestCase
 
 import bpy
 
 from .blender_manifest import BlenderManifest
 
-DEVELOPMENT_MODULE = ".".join(__name__.split(".")[:-2])
-MANIFEST_ID = BlenderManifest.read().id
+DEVELOPMENT_MODULE: Final = ".".join(__name__.split(".")[:-2])
+MANIFEST_ID: Final = BlenderManifest.read().id
+ESCAPE_START_CHAR: Final = "⟨"
+ESCAPE_END_CHAR: Final = "⟩"
 
 
 def make_test_method_name(text: str) -> str:
-    replaced_chars: list[str] = []
+    test_method_name = "test_"
     for char in text:
-        if char in ["⟨", "⟩"]:
-            replaced_chars.append(char * 2)
-        elif ("_" + char).isidentifier():
-            replaced_chars.append(char)
-        else:
-            replaced_chars.append(f"⟨{ord(char):x}⟩")
-    return "test_" + ("".join(replaced_chars))
+        if char in [ESCAPE_START_CHAR, ESCAPE_END_CHAR]:
+            test_method_name = f"{test_method_name}{char}{char}"
+            continue
+
+        appended_test_method_name = f"{test_method_name}{char}"
+        if appended_test_method_name.isidentifier():
+            test_method_name = appended_test_method_name
+            continue
+
+        test_method_name = (
+            f"{test_method_name}{ESCAPE_START_CHAR}{ord(char):x}{ESCAPE_END_CHAR}"
+        )
+
+    if not test_method_name.isidentifier():
+        message = f"Cannot convert to test method name: {text}"
+        raise ValueError(message)
+
+    return test_method_name
 
 
 class AddonTestCase(TestCase):
