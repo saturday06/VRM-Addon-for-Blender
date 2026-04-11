522567f28b89b729fe1827a369ebea81b6360bee
Isamu Mogi <isamu@leafytree.jp>
2026-04-12 06:52:47 +0900

    chore: make editor handler state private

--- src/io_scene_vrm/editor/handler.py
+++ src/io_scene_vrm/editor/handler.py
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: MIT OR GPL-3.0-or-later
 
+from dataclasses import dataclass, field
 from typing import Final
 
 import bpy
@@ -14,8 +15,17 @@ from .vrm1.ops import update_vrm1_expression_ui_list_elements
 _logger = get_logger(__name__)
 
 
-last_scene_names: Final[list[str]] = []
-last_armature_names: Final[list[str]] = []
+@dataclass(frozen=True)
+class State:
+    last_scene_names: list[str] = field(default_factory=list[str])
+    last_armature_names: list[str] = field(default_factory=list[str])
+
+    def clear(self) -> None:
+        self.last_scene_names.clear()
+        self.last_armature_names.clear()
+
+
+_state: Final = State()
 
 
 @persistent
@@ -29,16 +39,18 @@ def depsgraph_update_post(_scene: Scene, depsgraph: Depsgraph) -> None:
         depsgraph.id_type_updated("SCENE")
         and (True, scenes := context.blend_data.scenes)
         and (
-            len(scenes) != len(last_scene_names)
+            len(scenes) != len(_state.last_scene_names)
             or any(
                 last_scene_name != scene.name
-                for last_scene_name, scene in zip(last_scene_names, scenes)
+                for last_scene_name, scene in zip(_state.last_scene_names, scenes)
             )
         )
     ):
-        new_scenes = [scene for scene in scenes if scene.name not in last_scene_names]
-        last_scene_names.clear()
-        last_scene_names.extend(scene.name for scene in scenes)
+        new_scenes = [
+            scene for scene in scenes if scene.name not in _state.last_scene_names
+        ]
+        _state.last_scene_names.clear()
+        _state.last_scene_names.extend(scene.name for scene in scenes)
         for new_scene in new_scenes:
             get_scene_extension(new_scene).update_vrm0_material_property_names(context)
 
@@ -46,20 +58,22 @@ def depsgraph_update_post(_scene: Scene, depsgraph: Depsgraph) -> None:
         depsgraph.id_type_updated("ARMATURE")
         and (True, armatures := context.blend_data.armatures)
         and (
-            len(armatures) != len(last_armature_names)
+            len(armatures) != len(_state.last_armature_names)
             or any(
                 last_armature_name != armature.name
-                for last_armature_name, armature in zip(last_armature_names, armatures)
+                for last_armature_name, armature in zip(
+                    _state.last_armature_names, armatures
+                )
             )
         )
     ):
         new_armatures = [
             armature
             for armature in armatures
-            if armature.name not in last_armature_names
+            if armature.name not in _state.last_armature_names
         ]
-        last_armature_names.clear()
-        last_armature_names.extend(armature.name for armature in armatures)
+        _state.last_armature_names.clear()
+        _state.last_armature_names.extend(armature.name for armature in armatures)
         for new_armature in new_armatures:
             ext = get_armature_extension(new_armature)
             expressions = ext.vrm1.expressions
@@ -70,5 +84,4 @@ def depsgraph_update_post(_scene: Scene, depsgraph: Depsgraph) -> None:
 def clear_global_variables() -> None:
     migration.state.blend_file_compatibility_warning_shown = False
     migration.state.blend_file_addon_compatibility_warning_shown = False
-    last_scene_names.clear()
-    last_armature_names.clear()
+    _state.clear()
