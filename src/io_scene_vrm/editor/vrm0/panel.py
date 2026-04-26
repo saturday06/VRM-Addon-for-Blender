1ee830fccfb74e16a228f9e6df1ee81cd861b388
Isamu Mogi <isamu@leafytree.jp>
2026-04-26 18:27:36 +0900

    chore: label translation

--- src/io_scene_vrm/editor/vrm0/panel.py
+++ src/io_scene_vrm/editor/vrm0/panel.py
@@ -402,7 +402,7 @@ def _draw_vrm0_humanoid_layout(
     draw_vrm0_humanoid_required_bones_layout(armature, armature_box.box(), split_factor)
     draw_vrm0_humanoid_optional_bones_layout(armature, armature_box.box(), split_factor)
 
-    layout.label(text="Arm", icon="VIEW_PAN", translate=False)  # TODO: Translation
+    layout.label(text="Arm", icon="VIEW_PAN")
     layout.prop(
         humanoid,
         "arm_stretch",
