ae7d8832afb94bb2dd5ebc0ca724562984165450
Isamu Mogi <isamu@leafytree.jp>
2026-08-18 20:41:30 +0900

    chore: fix module name

--- tools/devcontainer_update_content_command.sh
+++ tools/devcontainer_update_content_command.sh
@@ -58,4 +58,7 @@ for blender_version in \
   ln -Tfs "${PWD}/src/io_scene_vrm" "${HOME}/.config/blender/$blender_version/scripts/addons/io_scene_vrm"
 done
 
-blender --background --python-expr 'import bpy; bpy.ops.preferences.addon_enable(module="bl_ext.user_default.vrm"); bpy.ops.wm.save_userpref()'
+blender \
+  --background \
+  --python-expr \
+  'import bpy; bpy.ops.preferences.addon_enable(module="bl_ext.user_default.vrm" if bpy.app.version >= (4, 2) else "io_scene_vrm"); bpy.ops.wm.save_userpref()'
