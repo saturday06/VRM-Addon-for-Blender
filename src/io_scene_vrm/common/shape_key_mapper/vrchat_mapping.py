820f0e83787ad92b360962b1a9f8a851540bc1e2
Isamu Mogi <isamu@leafytree.jp>
2026-09-04 14:16:03 +0000

    chore: more vrc shape keys

--- src/io_scene_vrm/common/shape_key_mapper/vrchat_mapping.py
+++ src/io_scene_vrm/common/shape_key_mapper/vrchat_mapping.py
@@ -8,10 +8,10 @@ from ..vrm1.expression_preset import ExpressionPreset, ExpressionPresets
 VRM1_PRESET_TO_VRCHAT_SHAPE_KEY_MAPPING: Final[
     Mapping[ExpressionPreset, Sequence[Mapping[str, float]]]
 ] = {
-    ExpressionPresets.AA: [{"vrc.v_aa": 1.0}],
-    ExpressionPresets.EE: [{"vrc.v_e": 1.0}],
-    ExpressionPresets.IH: [{"vrc.v_ih": 1.0}],
-    ExpressionPresets.OH: [{"vrc.v_oh": 1.0}],
-    ExpressionPresets.OU: [{"vrc.v_ou": 1.0}],
+    ExpressionPresets.AA: [{"vrc.v_aa": 1.0}, {"vrc_v_aa": 1.0}],
+    ExpressionPresets.EE: [{"vrc.v_e": 1.0}, {"vrc_v_e": 1.0}],
+    ExpressionPresets.IH: [{"vrc.v_ih": 1.0}, {"vrc_v_ih": 1.0}],
+    ExpressionPresets.OH: [{"vrc.v_oh": 1.0}, {"vrc_v_oh": 1.0}],
+    ExpressionPresets.OU: [{"vrc.v_ou": 1.0}, {"vrc_v_ou": 1.0}],
     ExpressionPresets.BLINK: [{"Blink": 1.0}],
 }
