58c3f2dfaa9c20897ce1c518b2afb100b6ba031e
Isamu Mogi <isamu@leafytree.jp>
2026-09-23 06:01:50 +0000

    chore: bpy.ops.file

--- typings/src/bpy-stubs/ops/__init__.pyi
+++ typings/src/bpy-stubs/ops/__init__.pyi
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: MIT OR GPL-3.0-or-later
 from bpy.ops import ed as ed
 from bpy.ops import export_scene as export_scene
+from bpy.ops import file as file
 from bpy.ops import import_scene as import_scene
 from bpy.ops import mesh as mesh
 from bpy.ops import object as object
